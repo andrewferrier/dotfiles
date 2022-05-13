791720c59e05c96531b723a048ee74971dcfbf37
Andrew Ferrier <andrewferrier@example.com>
2022-05-13 14:25:31 +0100

    feat: Different displays in different colors

--- macos/.config/swiftbar/yabai_space.1m.py
+++ macos/.config/swiftbar/yabai_space.1m.py
@@ -7,7 +7,25 @@
 import subprocess
 import json
 
+def find_display(space_index):
+    space_focused = [space['display'] for space in spaces if space['index'] == space_index][0]
+    return space_focused
+
+def find_color(space):
+    if space == space_focused:
+        return f"{BLACK}{space}{RESET}"
+    else:
+        display = find_display(space)
+
+        if display == 1:
+            return f"{BLUE}{space}{RESET}"
+        else:
+            return f"{GREEN}{space}{RESET}"
+
+
 BLUE = "\\\\e[0;34m"
+BLACK = "\\\\e[0;30m"
+GREEN = "\\\\e[0;35m"
 RESET = "\\\\e[0m"
 
 windows = json.loads(
@@ -24,19 +42,17 @@ spaces = json.loads(
 
 space_focused = [space for (space) in spaces if space["has-focus"]][0]["index"]
 
-spaces_display = [
+spaces_to_list = [
     window["space"]
     for (window) in windows
     if (not window["is-hidden"] and not window["is-minimized"])
 ] + [space_focused]
 
-spaces_display = sorted(set(spaces_display))
+spaces_to_list = sorted(set(spaces_to_list))
 
-spaces_display = map(
-    lambda space: f"{BLUE}{space}{RESET}"
-    if space == space_focused
-    else f"{space}",
-    spaces_display,
+spaces_to_list = map(
+    find_color,
+    spaces_to_list
 )
 
-print("🖥️ " + " ".join(spaces_display) + " | ansi=true")
+print("🖥️ " + " ".join(spaces_to_list) + " | ansi=true")
