f79634130eb464f175558b7689ec1483e55c681a
Andrew Ferrier <andrewferrier@example.com>
2022-05-14 09:34:01 +0100

    refactor: Migrate VimL → lua

--- common/.config/nvim/after/ftplugin/tex.lua
+++ common/.config/nvim/after/ftplugin/tex.lua
@@ -1,3 +1,19 @@
 -- luacheck: globals vim
 
 require("filetype.text").setup()
+
+local function create_pdf_from_tex(absoluteTexPath)
+    local directory = vim.fn.fnamemodify(absoluteTexPath, ":p:h")
+    local withoutExtension = vim.fn.fnamemodify(absoluteTexPath, ":p:r")
+
+    vim.cmd(
+        "!pdflatex -output-directory=" .. directory .. " " .. absoluteTexPath
+    )
+    vim.cmd("!open " .. withoutExtension .. ".pdf")
+end
+
+vim.keymap.set("n", "<Leader>cp", function()
+    create_pdf_from_tex(vim.fn.expand("%:p"))
+end, {
+    buffer = true,
+})
--- common/.config/nvim/after/ftplugin/tex.vim
+++
@@ -1,9 +0,0 @@
-function! CreatePdfFromTex(absoluteTexPath) abort
-    let l:directory        = fnamemodify(a:absoluteTexPath, ':p:h')
-    let l:withoutExtension = fnamemodify(a:absoluteTexPath, ':p:r')
-
-    execute '!pdflatex -output-directory=' . l:directory . ' ' . a:absoluteTexPath
-    silent execute '!open ' . l:withoutExtension . '.pdf'
-endfunction
-
-nnoremap <buffer> <Leader>cp :call CreatePdfFromTex(expand('%:p'))<CR>
