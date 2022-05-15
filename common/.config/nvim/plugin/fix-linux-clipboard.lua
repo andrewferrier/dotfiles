140b32e743485018fd60fa6b6991e40955dc39dc
Andrew Ferrier <andrewferrier@example.com>
2022-05-15 15:28:42 +0100

    fix: Use xsel for clipboard; more reliable

---
+++ common/.config/nvim/plugin/fix-linux-clipboard.lua
@@ -0,0 +1,21 @@
+-- luacheck: globals vim
+
+-- See https://github.com/neovim/neovim/issues/16503; clipboard does not seem to
+-- work correctly robustly with xclip (first call to `:registers` always shows
+-- an error. 'xclip' will be selected by default if it is installed, and other
+-- apps do appear to need it.
+
+if vim.loop.os_uname().sysname == "Linux" then
+    vim.g.clipboard = {
+        name = "xsel",
+        copy = {
+            ["*"] = { "xsel", "--nodetach", "-i", "-p" },
+            ["+"] = { "xsel", "--nodetach", "-i", "-b" },
+        },
+        paste = {
+            ["*"] = { "xsel", "-o", "-p" },
+            ["+"] = { "xsel", "-o", "-b" },
+        },
+        cache_enabled = 1,
+    }
+end
