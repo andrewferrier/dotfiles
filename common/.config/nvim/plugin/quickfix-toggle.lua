4ff9f6a3ef16efee05e2f193a67a9a0c00e4da9c
Andrew Ferrier <andrewferrier@example.com>
2022-05-20 21:35:30 +0100

    feat: Open quickfix automatically after 'grep'

--- common/.config/nvim/plugin/quickfix-toggle.lua
+++ common/.config/nvim/plugin/quickfix-toggle.lua
@@ -1,9 +1,16 @@
 -- luacheck: globals vim
 
+vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
+    group = vim.api.nvim_create_augroup("after_quick_fix", {}),
+    callback = function()
+        require("quickfix").open()
+    end,
+})
+
 vim.keymap.set("n", "yoq", function()
     if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then
-        require('quickfix').close()
+        require("quickfix").close()
     else
-        require('quickfix').open()
+        require("quickfix").open()
     end
 end)
