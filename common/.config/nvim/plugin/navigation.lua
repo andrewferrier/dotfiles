1c2d586342004dfff3cfa4c5ae9a2e34591b8796
Andrew Ferrier <andrewferrier@example.com>
2022-05-21 18:09:39 +0100

    fix: Check if in git dir for 'cdg'

--- common/.config/nvim/plugin/navigation.lua
+++ common/.config/nvim/plugin/navigation.lua
@@ -5,8 +5,13 @@ vim.opt.path:remove("/usr/include")
 local change_to_root = function()
     local dir = vim.fn.expand("%:p:h")
     local cmd = "cd " .. dir .. "; git rev-parse --show-toplevel"
-    local root = vim.fn.trim(vim.fn.system(cmd))
-    vim.cmd("lcd " .. root)
+    local root = vim.fn.system(cmd)
+
+    if vim.v.shell_error == 0 then
+        vim.cmd("lcd " .. vim.fn.trim(root))
+    else
+        vim.notify("Not in git directory.", vim.log.levels.ERROR)
+    end
 end
 
 vim.keymap.set("n", "cdf", ":lcd %:p:h<CR>", { silent = true })
