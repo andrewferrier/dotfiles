fc132a44227e871048476237baada54cd4441b30
Andrew Ferrier <andrewferrier@example.com>
2022-05-21 16:11:34 +0100

    feat: Improve warn & hint signs

--- common/.config/nvim/plugin/diagnostics.lua
+++ common/.config/nvim/plugin/diagnostics.lua
@@ -27,7 +27,7 @@ vim.diagnostic.config({
     },
 })
 
-local signs = { Error = "✘", Warn = "!", Hint = "H", Info = "I" }
+local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "I" }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
