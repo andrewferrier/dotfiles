93103982391c57880e256f7a4c8f8aa77ad546c6
Andrew Ferrier <andrewferrier@example.com>
2022-05-22 21:01:20 +0100

    feat: Change info icon

--- common/.config/nvim/plugin/diagnostics.lua
+++ common/.config/nvim/plugin/diagnostics.lua
@@ -27,7 +27,7 @@ vim.diagnostic.config({
     },
 })
 
-local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "I" }
+local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "ℹ"}
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
