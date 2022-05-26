e1df4d8dccd4327b14d4d544adc7887603ac2587
Andrew Ferrier <andrewferrier@example.com>
2022-05-26 17:41:13 +0100

    chore: Remove timezone plugin, I don't use it

--- macos/.config/swiftbar/timezones.1s.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# <swiftbar.hideAbout>true</swiftbar.hideAbout>
-# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
-# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
-
-echo -n "**PT** $(TZ=America/Los_Angeles date +'%H:%M')     "
-echo -n "**ET** $(TZ=America/New_York date +'%H:%M')"
-echo " | md=true"
