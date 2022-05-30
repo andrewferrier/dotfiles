335aa88c55b6b1a8d986dceb96146a9c8b0cd990
Andrew Ferrier <andrewferrier@example.com>
2022-05-30 17:10:07 +0100

    refactor: Convert Trash VimL → lua

---
+++ common/.config/nvim/pack/local/start/trash/plugin/trash.lua
@@ -0,0 +1,10 @@
+-- luacheck: globals vim
+
+vim.api.nvim_create_user_command("Trash", function(opts)
+   local file = vim.fn.fnamemodify(vim.fn.bufname(opts.args), ":p")
+   vim.cmd("bdelete" .. (opts.bang and "!" or ""))
+   vim.fn.system({ "trash", file })
+   if vim.fn.bufloaded(file) == 0 and vim.v.shell_error > 0 then
+      vim.notify("Couldn't move " .. file .. " to trash.", vim.log.levels.ERROR)
+   end
+end, { bang = true })
