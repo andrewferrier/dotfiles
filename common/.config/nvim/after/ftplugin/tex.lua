4674a6d68c8841c4680a5723b2ab9412aaaa19ad
Andrew Ferrier <andrewferrier@example.com>
2022-05-31 23:09:00 +0100

    refactor: vim.cmd → vim.fn.system

--- common/.config/nvim/after/ftplugin/tex.lua
+++ common/.config/nvim/after/ftplugin/tex.lua
@@ -6,10 +6,12 @@ local function create_pdf_from_tex(absoluteTexPath)
     local directory = vim.fn.fnamemodify(absoluteTexPath, ":p:h")
     local withoutExtension = vim.fn.fnamemodify(absoluteTexPath, ":p:r")
 
-    vim.cmd(
-        "!pdflatex -output-directory=" .. directory .. " " .. absoluteTexPath
-    )
-    vim.cmd("!open " .. withoutExtension .. ".pdf")
+    vim.fn.system({
+        "pdflatex",
+        "-output-directory=" .. directory,
+        absoluteTexPath,
+    })
+    vim.fn.system({ "open", withoutExtension .. ".pdf" })
 end
 
 vim.keymap.set("n", "<Leader>cp", function()
--- common/.config/nvim/pack/local/start/trash/plugin/trash.lua
+++ common/.config/nvim/pack/local/start/trash/plugin/trash.lua
@@ -1,10 +1,15 @@
 -- luacheck: globals vim
 
 vim.api.nvim_create_user_command("Trash", function(opts)
-   local file = vim.fn.fnamemodify(vim.fn.bufname(opts.args), ":p")
-   vim.cmd("bdelete" .. (opts.bang and "!" or ""))
-   vim.fn.system({ "trash", file })
-   if vim.fn.bufloaded(file) == 0 and vim.v.shell_error > 0 then
-      vim.notify("Couldn't move " .. file .. " to trash.", vim.log.levels.ERROR)
-   end
-end, { bang = true })
+    local file = vim.fn.fnamemodify(vim.fn.bufname(opts.args), ":p")
+    vim.api.nvim_buf_delete(0, { force = opts.bang })
+    vim.fn.system({ "trash", file })
+    if vim.fn.bufloaded(file) == 0 and vim.v.shell_error > 0 then
+        vim.notify(
+            "Couldn't move " .. file .. " to trash.",
+            vim.log.levels.ERROR
+        )
+    end
+end, {
+    bang = true,
+})
