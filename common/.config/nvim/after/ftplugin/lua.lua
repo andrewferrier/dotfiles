4e8e6d85107f2e5e63a4cd5542bf9d3908024ef2
Andrew Ferrier <andrewferrier@example.com>
2022-06-03 18:28:15 +0100

    feat: Add includeexpr to enable navigation of .lua files

--- common/.config/nvim/after/ftplugin/lua.lua
+++ common/.config/nvim/after/ftplugin/lua.lua
@@ -1,9 +1,12 @@
 -- luacheck: globals vim
 
 -- See https://www.reddit.com/r/neovim/comments/pl0p5v/comment/hc78cye/
-vim.opt_local.suffixesadd:prepend(".lua")
-vim.opt_local.suffixesadd:prepend("init.lua")
 vim.opt.path:append("," .. vim.fn.stdpath("config") .. "/**")
 vim.opt.path:append("," .. vim.fn.stdpath("data") .. "/**")
+vim.opt_local.suffixesadd:prepend(".lua")
+vim.opt_local.suffixesadd:prepend("init.lua")
+
+-- See https://www.reddit.com/r/neovim/comments/v3yv2x/comment/ib19q5j/
+vim.opt_local.includeexpr = 'substitute(v:fname, "\\\\.", "/", "g")'
 
 require("filetype.section").setup("^\\w\\(nd$\\)\\@!", "^\\(}\\|end\\)")
