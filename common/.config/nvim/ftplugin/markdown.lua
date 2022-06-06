2d113548ce4295a0f7eda435da0c681bf0b2fc2b
Andrew Ferrier <andrewferrier@example.com>
2022-06-06 22:10:02 +0100

    style: Format

--- common/.config/nvim/ftplugin/markdown.lua
+++ common/.config/nvim/ftplugin/markdown.lua
@@ -2,6 +2,6 @@
 -- https://github.com/tpope/vim-sleuth/issues/43#issuecomment-447207279;
 -- markdown should always use 4 spaces anyway.
 
-vim.b.sleuth_automatic=0
-vim.opt_local.shiftwidth=4
-vim.opt_local.tabstop=4
+vim.b.sleuth_automatic = 0
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
--- common/.config/nvim/ftplugin/terraform.lua
+++ common/.config/nvim/ftplugin/terraform.lua
@@ -1,5 +1,5 @@
-vim.b.sleuth_automatic=0
+vim.b.sleuth_automatic = 0
 
 -- Always use Terraform standard: https://www.terraform.io/language/syntax/style
-vim.opt_local.shiftwidth=2
-vim.opt_local.tabstop=2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
