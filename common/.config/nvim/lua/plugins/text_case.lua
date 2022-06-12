9e3d1c9b79caf996f0f2bea0ea239c9c4737002d
Andrew Ferrier <andrewferrier@example.com>
2022-06-12 08:55:40 +0100

    fix: Use custom keybindings for text_case

--- common/.config/nvim/lua/plugins/text_case.lua
+++ common/.config/nvim/lua/plugins/text_case.lua
@@ -1,3 +1,19 @@
-require("textcase").setup({
-    prefix = "gyc",
-})
+-- Do NOT run setup, otherwise it creates default keybindings
+-- require("textcase").setup({})
+
+local mapping = {}
+mapping["_"] = "to_snake_case"
+mapping["-"] = "to_dash_case"
+mapping["C"] = "to_constant_case"
+mapping["."] = "to_dot_case"
+mapping["c"] = "to_camel_case"
+mapping["t"] = "to_title_case"
+mapping["/"] = "to_path_case"
+mapping["s"] = "to_phrase_case" -- sentence
+mapping["m"] = "to_pascal_case" -- mixed case
+
+for map, target in pairs(mapping) do
+    vim.keymap.set("n", "gyc" .. map, function()
+        require("textcase").operator(target)
+    end)
+end
