fb69449f2a6d42a04b5f854c739977a09ec37e71
Andrew Ferrier <andrewferrier@example.com>
2022-06-13 22:40:14 +0100

    fix: Always use notify(), and override nvim_echo

--- common/.config/nvim/lua/plugins/notify.lua
+++ common/.config/nvim/lua/plugins/notify.lua
@@ -1,11 +1,17 @@
 if vim.fn.has("nvim-0.8.0") then
     vim.opt.cmdheight = 0
-
-    vim.notify = require("notify")
-
-    require("notify").setup({
-        timeout = 400,
-        render = "minimal",
-        stages = "fade",
-    })
 end
+
+vim.notify = require("notify")
+
+vim.api.nvim_echo = function(chunks, _, _)
+    for chunk = 1, #chunks do
+        vim.notify(chunks[chunk][1])
+    end
+end
+
+require("notify").setup({
+    timeout = 400,
+    render = "minimal",
+    stages = "fade",
+})
