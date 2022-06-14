cad01edcdfe3aae079bba686b8c19a4179db009d
Andrew Ferrier <andrewferrier@example.com>
2022-06-14 17:11:06 +0100

    fix: Link notify.nvim colors
    (so they are visible on a light background)

--- common/.config/nvim/plugin/colors.lua
+++ common/.config/nvim/plugin/colors.lua
@@ -15,6 +15,16 @@ end
 
 vim.cmd("colorscheme gruvbox")
 
+vim.api.nvim_set_hl(0, "NotifyERRORBorder", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyERRORIcon", { link = "DiagnosticError" })
+vim.api.nvim_set_hl(0, "NotifyERRORTitle", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyINFOBorder", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyINFOIcon", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyINFOTitle", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyWARNBorder", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NotifyWARNIcon", { link = "DiagnosticWarn" })
+vim.api.nvim_set_hl(0, "NotifyWARNTitle", { link = "Normal" })
+
 vim.api.nvim_set_hl(0, "QuickFixLine", { link = "CurSearch" })
 vim.api.nvim_set_hl(0, "StatusLineSecondary", { link = "SignColumn" })
 vim.api.nvim_set_hl(0, "TermCursorNC", { bg = "#00FF00", fg = "#FFFFFF" })
