8db0d305d4aa2abd9c56a431b6da60c775e5b9c4
Andrew Ferrier <andrewferrier@example.com>
2022-06-19 22:55:21 +0100

    refactor: Move cmdheight to display

--- common/.config/nvim/lua/plugins/notify.lua
+++ common/.config/nvim/lua/plugins/notify.lua
@@ -1,7 +1,3 @@
-if vim.fn.has("nvim-0.8.0") == 1 then
-    vim.opt.cmdheight = 0
-end
-
 vim.notify = function(msg, level, opts)
     msg = vim.fn.expand("%:t") .. ": " .. msg
     require("notify")(msg, level, opts)
--- common/.config/nvim/plugin/display.lua
+++ common/.config/nvim/plugin/display.lua
@@ -23,3 +23,7 @@ vim.opt.foldlevel = 99
 
 vim.opt.list = true
 vim.opt.listchars = "tab:>⋅,trail:·,extends:▷,precedes:◁,nbsp:␣"
+
+if vim.fn.has("nvim-0.8.0") == 1 then
+    vim.opt.cmdheight = 0
+end
