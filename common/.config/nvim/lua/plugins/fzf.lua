60f96a659a7666360f90aad48f3170a760ab3d47
Andrew Ferrier <andrewferrier@example.com>
2022-06-20 17:22:28 +0100

    refactor: VimL -> lua

--- common/.config/nvim/lua/plugins/fzf.lua
+++ common/.config/nvim/lua/plugins/fzf.lua
@@ -1,11 +1,24 @@
-vim.keymap.set("n", "cvh", ":Helptags<CR>", { silent = true })
-vim.keymap.set("n", "cvl", ":Rg<CR>", { silent = true })
+vim.keymap.set("n", "cvh", function()
+    vim.cmd("Helptags")
+end, { silent = true })
+
+vim.keymap.set("n", "cvl", function()
+    vim.cmd("Rg")
+end, { silent = true })
 
 vim.keymap.set("i", "<C-X><C-F>", "<Plug>(fzf-complete-path)")
 
 -- Supplement Maps from fzf.vim.
-vim.api.nvim_create_user_command("MapsI", ":call fzf#vim#maps('i', 0)", {})
-vim.api.nvim_create_user_command("MapsO", ":call fzf#vim#maps('o', 0)", {})
-vim.api.nvim_create_user_command("MapsX", ":call fzf#vim#maps('x', 0)", {})
+vim.api.nvim_create_user_command("MapsI", function()
+    vim.fn["fzf#vim#maps"]("i", 0)
+end, {})
+
+vim.api.nvim_create_user_command("MapsO", function()
+    vim.fn["fzf#vim#maps"]("o", 0)
+end, {})
+
+vim.api.nvim_create_user_command("MapsX", function()
+    vim.fn["fzf#vim#maps"]("x", 0)
+end, {})
 
 vim.g.fzf_layout = { window = { width = 0.9, height = 0.8 } }
