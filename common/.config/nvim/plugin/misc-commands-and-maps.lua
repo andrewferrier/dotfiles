94c569adf77e4b5b8c51a1b3c3c0fb58c63ba315
Andrew Ferrier <andrewferrier@example.com>
2022-06-20 19:40:25 +0100

    chore: Re-enable keys
    These are harmless and I might even use them sometime.

--- common/.config/nvim/plugin/misc-commands-and-maps.lua
+++ common/.config/nvim/plugin/misc-commands-and-maps.lua
@@ -3,7 +3,4 @@ vim.api.nvim_create_user_command("ReadonlyEffective", function()
     vim.notify("Effective R/O on.")
 end, {})
 
-vim.keymap.set("n", "Q", "<Nop>")
-vim.keymap.set("n", "<C-Z>", "<Nop>")
-
 vim.keymap.set("n", "<Leader>r", ':Rename <C-R>=expand("%:t")<CR>')
