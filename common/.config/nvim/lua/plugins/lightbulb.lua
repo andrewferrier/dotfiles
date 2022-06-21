79b79836ac97e50ee1e9fab6841d1a889ebe7e99
Andrew Ferrier <andrewferrier@example.com>
2022-06-21 23:15:17 +0100

    fix: Reduce padding on lightbulb indicator

--- common/.config/nvim/lua/plugins/lightbulb.lua
+++ common/.config/nvim/lua/plugins/lightbulb.lua
@@ -3,5 +3,5 @@ vim.g.cursorhold_updatetime = 100
 require("nvim-lightbulb").setup({
     sign = { enabled = false },
     autocmd = { enabled = true },
-    virtual_text = { enabled = true, text = "     α" },
+    virtual_text = { enabled = true, text = " α" },
 })
