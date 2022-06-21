462805b89e53bd77155a02c0ae36f86dcb5c2e68
Andrew Ferrier <andrewferrier@example.com>
2022-06-21 23:31:47 +0100

    refactor: Reorganize plugin/

--- common/.config/nvim/plugin/completion.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.completeopt:remove("noinsert")
-vim.opt.completeopt:remove("menuone")
-vim.opt.completeopt:append("preview") -- Doesn't reliably close
--- common/.config/nvim/plugin/editing.lua
+++ common/.config/nvim/plugin/editing.lua
@@ -1,1 +1,5 @@
+vim.opt.completeopt:remove("noinsert")
+vim.opt.completeopt:remove("menuone")
+vim.opt.completeopt:append("preview") -- Doesn't reliably close
+
 vim.opt.nrformats:append("unsigned")
