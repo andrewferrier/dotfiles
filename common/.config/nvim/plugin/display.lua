9c44a5736e1949e4ded84d598810624caaeb1e88
Andrew Ferrier <andrewferrier@example.com>
2022-06-25 01:02:59 +0100

    docs: Disable cmdheight = 0 (for now)

--- common/.config/nvim/plugin/display.lua
+++ common/.config/nvim/plugin/display.lua
@@ -24,6 +24,7 @@ vim.opt.foldlevel = 99
 vim.opt.list = true
 vim.opt.listchars = "tab:>⋅,trail:·,extends:▷,precedes:◁,nbsp:␣"
 
-if vim.fn.has("nvim-0.8.0") == 1 then
-    vim.opt.cmdheight = 0
-end
+-- TODO: Re-enable this once https://github.com/neovim/neovim/pull/18961 is merged
+-- if vim.fn.has("nvim-0.8.0") == 1 then
+--     vim.opt.cmdheight = 0
+-- end
