40b4e4cfa7333be2f6419adecf675b2c73241dac
Andrew Ferrier <andrewferrier@example.com>
2022-06-27 10:35:34 +0100

    style: Format

--- common/.config/nvim/lua/plugins/gitsigns.lua
+++ common/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,15 @@
 local gitsigns = require("gitsigns")
 
 local function visual_stage()
-    local first_line = vim.fn.line('v')
-    local last_line = vim.fn.getpos('.')[2]
+    local first_line = vim.fn.line("v")
+    local last_line = vim.fn.getpos(".")[2]
     gitsigns.stage_hunk({ first_line, last_line })
     -- Switch back to normal mode, there may be a cleaner way to do this
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 't', false)
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+        "t",
+        false
+    )
 end
 
 local attach = function(bufnr)
