d2c4261a18369c91f8d8567865551d231bc28b96
Andrew Ferrier <andrewferrier@example.com>
2022-06-29 18:14:03 +0100

    feat: Make fidget more subtle

--- common/.config/nvim/lua/plugins/fidget.lua
+++ common/.config/nvim/lua/plugins/fidget.lua
@@ -3,4 +3,16 @@ require("fidget").setup({
         fidget_decay = 500,
         task_decay = 500,
     },
+    text = {
+        spinner = "star",
+    },
+    fmt = {
+        task = function(task_name, _, percentage)
+            return string.format(
+                "%s [%s]",
+                percentage and string.format(" (%s%%)", percentage) or "",
+                task_name
+            )
+        end,
+    },
 })
