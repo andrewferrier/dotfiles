d3b8a6ed9f769c17da86ae02f8def0114b5fdab8
Andrew Ferrier <andrewferrier@example.com>
2022-07-07 18:46:04 +0100

    refactor: Enable filetype.lua by default in 0.8+

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,9 +1,13 @@
 -- See ~/.config/nvim/filetype.lua for custom filetype overrides
 
--- Use exclusively filetype.lua file detection, see
--- https://gpanders.com/blog/whats-new-in-neovim-0-7/
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
+-- In NeoVim 0.8+ this is the default - see
+-- https://github.com/neovim/neovim/issues/14090#issuecomment-1177933661
+if vim.fn.has("nvim-0.8.0") ~= 1 then
+    -- Use exclusively filetype.lua file detection, see
+    -- https://gpanders.com/blog/whats-new-in-neovim-0-7/
+    vim.g.do_filetype_lua = 1
+    vim.g.did_load_filetypes = 0
+end
 
 -- Based on
 -- https://neovim.discourse.group/t/how-to-disable-builtin-plugins/787/5
