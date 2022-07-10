8a0d5fadb655f5e0152112ccda68945a5f7eda86
Andrew Ferrier <andrewferrier@example.com>
2022-07-10 11:58:40 +0100

    feat: Use default keymaps for nvim-surround

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -1,11 +1,1 @@
-vim.keymap.set('n', 's', '<Nop>')
-vim.keymap.set('x', 's', '<Nop>')
-
-require("nvim-surround").setup({
-    keymaps = {
-        insert = "sa",
-        visual = "s",
-        delete = "sd",
-        change = "sr",
-    },
-})
+require("nvim-surround").setup({})
