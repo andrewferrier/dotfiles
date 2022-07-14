c649373f733ee7b1a4e315f497f404f0a4a9e45a
Andrew Ferrier <andrewferrier@example.com>
2022-07-14 10:34:32 +0100

    refactor: Simplify switching back to normal mode

--- common/.config/nvim/lua/plugins/gitsigns.lua
+++ common/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,12 +4,11 @@ local function visual_stage()
     local first_line = vim.fn.line("v")
     local last_line = vim.fn.getpos(".")[2]
     gitsigns.stage_hunk({ first_line, last_line })
-    -- Switch back to normal mode, there may be a cleaner way to do this
-    vim.api.nvim_feedkeys(
-        vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
-        "t",
-        false
-    )
+
+    local mode = vim.fn.mode():lower()
+    if mode:find("^v") or mode:find("^ctrl-v") then
+        vim.cmd("normal! V")
+    end
 end
 
 local attach = function(bufnr)
