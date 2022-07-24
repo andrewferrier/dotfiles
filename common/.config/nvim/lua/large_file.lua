1cb855f8cb3c01b025313ccf3ec0cd7364231351
Andrew Ferrier <andrewferrier@example.com>
2022-07-24 23:25:06 +0100

    fix: Set gitsigns large file size consistently

--- common/.config/nvim/lua/large_file.lua
+++ common/.config/nvim/lua/large_file.lua
@@ -1,11 +1,13 @@
 local M = {}
 
+M.LARGE_FILE_LINE_COUNT = 10000
+
 M.is_large_file = function(bufnr)
     if bufnr == nil then
         bufnr = 0
     end
 
-    return (vim.api.nvim_buf_line_count(bufnr) > 10000)
+    return (vim.api.nvim_buf_line_count(bufnr) > M.LARGE_FILE_LINE_COUNT)
 end
 
 return M
