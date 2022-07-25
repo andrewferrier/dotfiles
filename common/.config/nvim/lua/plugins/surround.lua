097102745749e35f30d711cc88ee67774a1bffcf
Andrew Ferrier <andrewferrier@example.com>
2022-07-25 14:22:29 +0100

    fix: Add warnings for sa/sd/sr

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -10,3 +10,19 @@ require("nvim-surround.buffer").format_lines = function(_, _)
     -- Disable indenting by nvim-surround; see
     -- https://github.com/kylechui/nvim-surround/issues/109#issuecomment-1194034971
 end
+
+local warn = function()
+    vim.notify("Don't use 's'; use 'ys', 'ds', or 'cs'!", vim.log.levels.WARN)
+end
+
+vim.keymap.set("n", "sa", function()
+    warn()
+end)
+
+vim.keymap.set("n", "sd", function()
+    warn()
+end)
+
+vim.keymap.set("n", "sr", function()
+    warn()
+end)
