7d24d8e1ae705e337f793cc344b6e2faf48b0837
Andrew Ferrier <andrewferrier@example.com>
2022-07-25 14:22:11 +0100

    fix: Temporary workaround for nvim-surround indent

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -5,3 +5,8 @@ require("nvim-surround").setup({
         end,
     },
 })
+
+require("nvim-surround.buffer").format_lines = function(_, _)
+    -- Disable indenting by nvim-surround; see
+    -- https://github.com/kylechui/nvim-surround/issues/109#issuecomment-1194034971
+end
