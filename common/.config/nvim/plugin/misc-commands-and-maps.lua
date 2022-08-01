e6c399f5f61702d1dfc02ad247dd4e280a86ecc0
Andrew Ferrier <andrewferrier@example.com>
2022-08-01 16:22:49 +0100

    feat: Use <CR> to :update

--- common/.config/nvim/plugin/misc-commands-and-maps.lua
+++ common/.config/nvim/plugin/misc-commands-and-maps.lua
@@ -1,1 +1,9 @@
 vim.keymap.set("n", "<Leader>r", ':Rename <C-R>=expand("%:t")<CR>')
+
+vim.keymap.set("n", "<CR>", function()
+    if vim.o.buftype == "" and vim.o.modified == true then
+        return ":update<CR>"
+    else
+        return "<CR>"
+    end
+end, { expr = true })
