e5551a0eddf9ff7d112e9552ace9042f4ff372bb
Andrew Ferrier <andrewferrier@example.com>
2022-08-05 14:42:39 +0100

    feat: Add keys for amend and 'git open'

--- common/.config/nvim/plugin/git.lua
+++ common/.config/nvim/plugin/git.lua
@@ -6,6 +6,10 @@ vim.keymap.set("n", "gbc", function()
     require("open_terminal_fm").open_terminal("git commit", "%:p:h")
 end, opts)
 
+vim.keymap.set("n", "gbm", function()
+    require("open_terminal_fm").open_terminal("git commit --amend", "%:p:h")
+end, opts)
+
 vim.keymap.set("n", "gbp", function()
     require("open_terminal_fm").open_terminal("git push", "%:p:h")
 end, opts)
@@ -16,6 +20,10 @@ end, opts)
 
 vim.keymap.set("n", "gbt", function()
     require("open_terminal_fm").open_terminal("tig", "%:p:h")
+end, opts)
+
+vim.keymap.set("n", "gbo", function()
+    require("open_terminal_fm").open_terminal("git open", "%:p:h")
 end, opts)
 
 vim.keymap.set("n", "[C", "gg]c", { remap = true })
