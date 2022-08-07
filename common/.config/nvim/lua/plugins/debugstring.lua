ac8e11685b30ccbc43d7c539c0cf5949a771a8d0
Andrew Ferrier <andrewferrier@example.com>
2022-08-07 23:22:49 +0100

    feat: Start work on debugprint

--- common/.config/nvim/lua/plugins/debugstring.lua
+++ common/.config/nvim/lua/plugins/debugstring.lua
@@ -1,10 +1,10 @@
 -- Mnemonic: dqp = debug quickly print
-vim.keymap.set("n", "dqp", "<Plug>DumpDebugStringVar", { unique = true })
-vim.keymap.set("n", "dqP", "k<Plug>DumpDebugStringVar", { unique = true })
-vim.keymap.set("n", "dQp", "<Plug>DumpDebugStringExpr", { unique = true })
-vim.keymap.set("n", "dQP", "k<Plug>DumpDebugStringExpr", { unique = true })
+-- vim.keymap.set("n", "dqp", "<Plug>DumpDebugStringVar", { unique = true })
+-- vim.keymap.set("n", "dqP", "k<Plug>DumpDebugStringVar", { unique = true })
+-- vim.keymap.set("n", "dQp", "<Plug>DumpDebugStringExpr", { unique = true })
+-- vim.keymap.set("n", "dQP", "k<Plug>DumpDebugStringExpr", { unique = true })
 
--- Work around https://github.com/bergercookie/vim-debugstring/issues/16
-vim.opt.runtimepath:append(
-    vim.fn.stdpath("data") .. "/site/pack/packer/start/vim-repeat"
-)
+-- -- Work around https://github.com/bergercookie/vim-debugstring/issues/16
+-- vim.opt.runtimepath:append(
+--     vim.fn.stdpath("data") .. "/site/pack/packer/start/vim-repeat"
+-- )
