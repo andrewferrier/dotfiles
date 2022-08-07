c8160eac03807ce2e852c569e5e1476483ab0359
Andrew Ferrier <andrewferrier@example.com>
2022-08-07 13:03:19 +0100

    feat: Add find/change/delete to markdown 'l'

--- common/.config/nvim/after/ftplugin/markdown.lua
+++ common/.config/nvim/after/ftplugin/markdown.lua
@@ -11,9 +11,24 @@ require("nvim-surround").buffer_setup({
     surrounds = {
         ["l"] = {
             add = function()
-                local register = vim.fn.getreg("*"):gsub("\n", "")
-                return { { "[" }, { "](" .. register .. ")" } }
+                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                return {
+                    { "[" },
+                    { "](" .. clipboard .. ")" },
+                }
             end,
+            find = "%b[]%b()",
+            delete = "^(%[)().-(%]%b())()$",
+            change = {
+                target = "^()()%b[]%((.-)()%)$",
+                replacement = function()
+                    local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                    return {
+                        { "" },
+                        { clipboard },
+                    }
+                end,
+            },
         },
         ["L"] = {
             add = function()
