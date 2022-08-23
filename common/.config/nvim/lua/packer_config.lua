5937f95eb1a1dfe8d5ae475954a60c8233a9f26a
Andrew Ferrier <andrewferrier@example.com>
2022-08-23 14:12:59 +0100

    feat: Enable autolist plugin

--- common/.config/nvim/lua/packer_config.lua
+++ common/.config/nvim/lua/packer_config.lua
@@ -165,6 +165,13 @@ local function packages_syntax(use)
     })
 
     use({ "jkramer/vim-checkbox" })
+
+    use({
+        "gaoDean/autolist.nvim",
+        config = function()
+            require("autolist").setup({})
+        end,
+    })
 end
 
 local function packages_treesitter(use)
