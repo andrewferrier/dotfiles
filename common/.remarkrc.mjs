d4175ba0702751cd8d233ff6c89dc9278680ef3a
Andrew Ferrier <andrewferrier@example.com>
2022-08-25 19:07:28 +0100

    feat: Add frontmatter support

--- common/.remarkrc.mjs
+++ common/.remarkrc.mjs
@@ -1,5 +1,6 @@
 const remarkConfig = {
   plugins: [
+    process.env.NPM_PACKAGES + '/lib/node_modules/remark-frontmatter/index.js',
     process.env.NPM_PACKAGES + '/lib/node_modules/remark-gfm/index.js'
   ]
 }
