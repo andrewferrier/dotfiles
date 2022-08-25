be614a00a1784d2093284ae1507c526a6562b3d8
Andrew Ferrier <andrewferrier@example.com>
2022-08-25 19:06:26 +0100

    feat(mdl): Don't warn on first top-level

--- if-command/mdl/.config/mdl/.mdl.rb
+++ if-command/mdl/.config/mdl/.mdl.rb
@@ -8,4 +8,5 @@ rule 'MD046', :style => :consistent
 exclude_rule 'MD009' # use null_ls instead, it's more accurate
 exclude_rule 'MD013'
 exclude_rule 'MD024' # it is valid to have multiple headers with the same content in multiple sections!
+exclude_rule 'MD041' # it is not necessarily true that the first line should be top-level, e.g. frontmatter
 exclude_rule 'MD033'
