6c9c94922373a613dbcf318c91a888fd31f6df43
Andrew Ferrier <andrewferrier@example.com>
2022-08-26 20:04:33 +0100

    fix: Disable markdown text objects
    nvim-treesitter now provides native folding support

--- common/.config/nvim/plugin/folding.lua
+++ common/.config/nvim/plugin/folding.lua
@@ -2,9 +2,7 @@ local set_fold_method = function()
     local large_file = require("large_file").is_large_file()
     local treesitter_parsers = require("nvim-treesitter.parsers")
 
-    if vim.opt.filetype:get() == "markdown" then
-        vim.opt_local.foldmethod = 'manual'
-    elseif vim.wo.foldmethod == "manual" then
+    if vim.wo.foldmethod == "manual" then
         if not large_file and treesitter_parsers.has_parser() then
             vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
             vim.opt_local.foldmethod = 'expr'
