51540ea5ebce548e830354e7d51d7a2c44a01c66
Andrew Ferrier <andrewferrier@example.com>
2022-08-30 16:25:16 +0100

    fix: How antidote functions are loaded

--- if-command/zsh/.config/zsh.d/50-load-plugins.zsh
+++ if-command/zsh/.config/zsh.d/50-load-plugins.zsh
@@ -1,3 +1,3 @@
 source ${ZSH_CACHE_FOLDER}/plugins.sh
 # Support commands like `antidote update`
-autoload -Uz ${ZSH_CACHE_FOLDER}/antidote/functions/antidote
+autoload -Uz ${ZSH_CACHE_FOLDER}/antidote/functions/*
