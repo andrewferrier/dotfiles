66dc0c8113d9e89408fa79549bc5036fc52abe5e
Andrew Ferrier <andrewferrier@example.com>
2022-09-01 11:22:08 +0100

    refactor: Simplify make-notes

--- common/.config/nvim/plugin/make-notes.lua
+++ common/.config/nvim/plugin/make-notes.lua
@@ -1,22 +1,22 @@
 local set = function(lhs, rhs)
-    vim.keymap.set("n", lhs, rhs, {})
+    vim.keymap.set("n", "<Leader>" .. lhs, rhs, {})
 end
 
 local DATE = "=strftime('%F')"
 local DATETIME = "=strftime('%FT%T%z')"
 
-set("<Leader>n.", ":split ./<C-R>" .. DATE .. "<CR>-")
-set("<Leader>nd", ":split ~/Desktop/<C-R>" .. DATE .. "<CR>-")
-set("<Leader>nm", ":split ~/Documents/meetings/<C-R>" .. DATE .. "<CR>-")
-set("<Leader>nn", ":split ~/notes/<C-R>" .. DATE .. "<CR>-")
+set("n.", ":split ./<C-R>" .. DATE .. "<CR>-")
+set("nd", ":split ~/Desktop/<C-R>" .. DATE .. "<CR>-")
+set("nm", ":split ~/Documents/meetings/<C-R>" .. DATE .. "<CR>-")
+set("nn", ":split ~/notes/<C-R>" .. DATE .. "<CR>-")
 
-set("<Leader>nT", ":split /tmp/<C-R>" .. DATETIME .. "<CR>.md<CR>")
-set("<Leader>nt", ":split /tmp/<C-R>" .. DATETIME .. "<CR>-")
+set("nT", ":split /tmp/<C-R>" .. DATETIME .. "<CR>.md<CR>")
+set("nt", ":split /tmp/<C-R>" .. DATETIME .. "<CR>-")
 
-set("<Leader>s.", ":save ./<C-R>" .. DATE .. "<CR>-")
-set("<Leader>sd", ":save ~/Desktop/<C-R>" .. DATE .. "<CR>-")
-set("<Leader>sm", ":save ~/Documents/meetings/<C-R>" .. DATE .. "<CR>-")
-set("<Leader>sn", ":save ~/notes/<C-R>" .. DATE .. "<CR>-")
+set("s.", ":save ./<C-R>" .. DATE .. "<CR>-")
+set("sd", ":save ~/Desktop/<C-R>" .. DATE .. "<CR>-")
+set("sm", ":save ~/Documents/meetings/<C-R>" .. DATE .. "<CR>-")
+set("sn", ":save ~/notes/<C-R>" .. DATE .. "<CR>-")
 
-set("<Leader>sT", ":save /tmp/<C-R>" .. DATETIME .. "<CR>.md<CR>")
-set("<Leader>st", ":save /tmp/<C-R>" .. DATETIME .. "<CR>-")
+set("sT", ":save /tmp/<C-R>" .. DATETIME .. "<CR>.md<CR>")
+set("st", ":save /tmp/<C-R>" .. DATETIME .. "<CR>-")
