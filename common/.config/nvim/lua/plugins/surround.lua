3f47b5bed0818a1bbaf597f9d57a876ff976a8aa
Andrew Ferrier <andrewferrier@example.com>
2022-09-04 17:34:45 +0100

    refactor: indent_lines using built-in capability

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -8,12 +8,8 @@ require("nvim-surround").setup({
         delete = "sd",
         change = "sr",
     },
+    indent_lines = false,
 })
-
-require("nvim-surround.buffer").format_lines = function(_, _)
-    -- Disable indenting by nvim-surround; see
-    -- https://github.com/kylechui/nvim-surround/issues/109#issuecomment-1194034971
-end
 
 local warn = function()
     vim.notify("Don't use 'ys', 'ds', or 'cs'!", vim.log.levels.WARN)
