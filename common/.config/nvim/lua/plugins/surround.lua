7ad78c7c9ac9c3e05f77b4f5098af366e22e381d
Andrew Ferrier <andrewferrier@example.com>
2022-09-04 17:32:09 +0100

    Revert "feat: Use default keymaps for nvim-surround"
    This reverts commit 8a0d5fadb655f5e0152112ccda68945a5f7eda86.
    
    I've decided I prefer 's' prefixes and they are more consistent/have
    better mnemonics.

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -1,4 +1,14 @@
-require("nvim-surround").setup()
+vim.keymap.set("n", "s", "<Nop>")
+vim.keymap.set("x", "s", "<Nop>")
+
+require("nvim-surround").setup({
+    keymaps = {
+        normal = "sa",
+        visual = "s",
+        delete = "sd",
+        change = "sr",
+    },
+})
 
 require("nvim-surround.buffer").format_lines = function(_, _)
     -- Disable indenting by nvim-surround; see
@@ -6,17 +16,17 @@ require("nvim-surround.buffer").format_lines = function(_, _)
 end
 
 local warn = function()
-    vim.notify("Don't use 's'; use 'ys', 'ds', or 'cs'!", vim.log.levels.WARN)
+    vim.notify("Don't use 'ys', 'ds', or 'cs'!", vim.log.levels.WARN)
 end
 
-vim.keymap.set("n", "sa", function()
+vim.keymap.set("n", "ys", function()
     warn()
 end)
 
-vim.keymap.set("n", "sd", function()
+vim.keymap.set("n", "ds", function()
     warn()
 end)
 
-vim.keymap.set("n", "sr", function()
+vim.keymap.set("n", "cs", function()
     warn()
 end)
