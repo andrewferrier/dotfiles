1c7b43b80bfb2025078b3b8b4c2c72e27059bac1
Andrew Ferrier <andrewferrier@example.com>
2022-09-04 13:25:07 +0100

    Revert "refactor: Replace my homegrown TODO"
    This reverts commit 78ced3b4e3d45a2559fa4c6487c14e890ce624f7.
    
    Fails with Makefiles that contain comments.

--- common/.config/nvim/lua/plugins/null_ls/sources.lua
+++ common/.config/nvim/lua/plugins/null_ls/sources.lua
@@ -1,6 +1,58 @@
 local M = {}
 
 local null_ls = require("null-ls")
+local null_ls_helpers = require("null-ls.helpers")
+
+local SEVERITIES = null_ls_helpers.diagnostics.severities
+
+local diagnostics_by_line_results = function(content, match_regex, name)
+    local result = {}
+
+    for line_number = 1, #content do
+        match_regex(function(regex, message, severity)
+            local line = content[line_number]
+            local start_byte, end_byte = regex:match_str(line)
+
+            if start_byte ~= nil then
+                table.insert(result, {
+                    message = message,
+                    severity = severity,
+                    row = line_number,
+                    col = start_byte + 1,
+                    end_row = line_number,
+                    end_col = end_byte + 1,
+                    source = name,
+                })
+            end
+        end)
+    end
+
+    return result
+end
+
+local diagnostics_by_line = function(options)
+    return {
+        method = null_ls.methods.DIAGNOSTICS,
+        name = options.name,
+        filetypes = {},
+        disabled_filetypes = options.disabled_filetypes,
+        generator = {
+            async = true,
+            fn = function(params, done)
+                return done(
+                    diagnostics_by_line_results(
+                        params.content,
+                        options.fn,
+                        options.name
+                    )
+                )
+            end,
+        },
+    }
+end
+
+local REGEX_TODO = vim.regex("TODO")
+local REGEX_FIXME = vim.regex("FIXME")
 
 local whitespace_str = function(length)
     local indent_str = ""
@@ -107,12 +159,6 @@ M.sources = {
     }),
     null_ls.builtins.diagnostics.sqlfluff,
     null_ls.builtins.diagnostics.tidy,
-    null_ls.builtins.diagnostics.todo_comments.with({
-        -- disabled for 'gitcommit'/'gitrebase' because it doesn't make any
-        -- sense, sometimes we will want to write TODO and that's a valid part
-        -- of the message.
-        disabled_filetypes = { "dirbuf", "gitcommit", "gitrebase" },
-    }),
     null_ls.builtins.diagnostics.trail_space.with({
         -- for 'mail' it's just annoying
         -- for 'markdown' it's more accurate than mdl
@@ -125,6 +171,17 @@ M.sources = {
     }),
     null_ls.builtins.diagnostics.vale,
     null_ls.builtins.diagnostics.zsh,
+    diagnostics_by_line({
+        name = "todo-fixme",
+        -- disabled for 'gitcommit'/'gitrebase' because it doesn't make any
+        -- sense, sometimes we will want to write TODO and that's a valid part
+        -- of the message.
+        disabled_filetypes = { "dirbuf", "gitcommit", "gitrebase" },
+        fn = function(match_regex)
+            match_regex(REGEX_TODO, "TODO", SEVERITIES.hint)
+            match_regex(REGEX_FIXME, "FIXME", SEVERITIES.hint)
+        end,
+    }),
 }
 
 return M
