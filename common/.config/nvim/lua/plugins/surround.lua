51480c0361185a26e6329c7a2ffcc208c151c949
Andrew Ferrier <andrewferrier@example.com>
2022-09-04 17:34:57 +0100

    refactor: Simplify logic

--- common/.config/nvim/lua/plugins/surround.lua
+++ common/.config/nvim/lua/plugins/surround.lua
@@ -15,14 +15,6 @@ local warn = function()
     vim.notify("Don't use 'ys', 'ds', or 'cs'!", vim.log.levels.WARN)
 end
 
-vim.keymap.set("n", "ys", function()
-    warn()
-end)
-
-vim.keymap.set("n", "ds", function()
-    warn()
-end)
-
-vim.keymap.set("n", "cs", function()
-    warn()
-end)
+vim.keymap.set("n", "ys", warn)
+vim.keymap.set("n", "ds", warn)
+vim.keymap.set("n", "cs", warn)
