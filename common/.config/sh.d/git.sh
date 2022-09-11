099ab4be4b0539fad21be54a7638ef1cf57d3937
Andrew Ferrier <andrewferrier@example.com>
2022-09-11 18:39:23 +0100

    refactor: Stop using egrep

--- common/.config/sh.d/git.sh
+++ common/.config/sh.d/git.sh
@@ -6,5 +6,5 @@ function git-commit-submodule() {
 }
 
 # shellcheck disable=SC2142
-alias git-prune-local="git fetch --prune; git branch -r | awk '{print \$1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print \$1}' | xargs git branch -d;"
+alias git-prune-local="git fetch --prune; git branch -r | awk '{print \$1}' | grep -E -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print \$1}' | xargs git branch -d;"
 alias gpc='while true; do git add -i -p; if ! git commit; then break; fi; done'
