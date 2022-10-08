ddc805d008be37d9039ae3bd9df89e4af8fa5a33
Andrew Ferrier <andrewferrier@example.com>
2022-10-08 16:58:30 +0100

    feat: Set splitkeep option when available

--- common/.config/nvim/plugin/display.lua
+++ common/.config/nvim/plugin/display.lua
@@ -6,6 +6,10 @@ vim.opt.sidescrolloff = 15
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+if vim.fn.has("nvim-0.9.0") == 1 then
+    vim.opt.splitkeep = "screen"
+end
+
 -- Fix https://github.com/neovim/neovim/issues/12288#issuecomment-626276240; I
 -- also find it interesting to see the file summary when opening it.
 vim.opt.shortmess:append("c")
