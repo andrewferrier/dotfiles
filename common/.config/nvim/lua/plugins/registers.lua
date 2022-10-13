2ea54c5ce36b5613146ace7bc5715b1ec4dfab06
Andrew Ferrier <andrewferrier@example.com>
2022-10-13 21:41:58 +0100

    fix: Handle newer registers setup

--- common/.config/nvim/lua/plugins/registers.lua
+++ common/.config/nvim/lua/plugins/registers.lua
@@ -1,5 +1,6 @@
-vim.g.registers_delay = 0
-vim.g.registers_hide_only_whitespace = 1
-vim.g.registers_show = "*-/#%.0123456789abcdefghijklmnopqrstuvwxyz"
-vim.g.registers_show_empty_registers = 0
-vim.g.registers_window_border = "rounded"
+require("registers").setup({
+    show = "*-/#%.0123456789abcdefghijklmnopqrstuvwxyz",
+    show_empty = false,
+    trim_whitespace = true,
+    window = { border = "rounded", transparency = 0 },
+})
