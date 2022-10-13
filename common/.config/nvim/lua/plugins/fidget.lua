453bddda1b39862e9905725565d74d2a5537e4ea
Andrew Ferrier <andrewferrier@example.com>
2022-10-13 17:00:11 +0100

    style: Format

--- common/.config/nvim/lua/plugins/fidget.lua
+++ common/.config/nvim/lua/plugins/fidget.lua
@@ -24,7 +24,4 @@ require("fidget").setup({
 })
 
 -- Workaround for bug here: https://github.com/j-hui/fidget.nvim/issues/86
-vim.api.nvim_create_autocmd(
-    "VimLeavePre",
-    { command = [[silent! FidgetClose]] }
-)
+vim.api.nvim_create_autocmd("VimLeavePre", { command = "silent! FidgetClose" })
