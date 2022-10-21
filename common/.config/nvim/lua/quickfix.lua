3c5a0d9980290b9446084b02ac1fe894761172c2
Andrew Ferrier <andrewferrier@example.com>
2022-10-21 22:50:41 +0100

    refactor: Inline variable

--- common/.config/nvim/lua/quickfix.lua
+++ common/.config/nvim/lua/quickfix.lua
@@ -5,9 +5,7 @@ M.close = function()
 end
 
 M.open = function()
-    local list = vim.fn.getqflist()
-
-    if vim.tbl_count(list) == 0 then
+    if vim.tbl_count(vim.fn.getqflist()) == 0 then
         vim.notify(
             "Nothing in quickfix list; not opening.",
             vim.log.levels.WARN
