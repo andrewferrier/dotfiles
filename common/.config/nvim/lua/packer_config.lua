33e5c1416b4e8ed2b56120fdc21938963c371e86
Andrew Ferrier <andrewferrier@example.com>
2022-10-28 10:54:57 +0100

    docs: Add explanatory comment

--- common/.config/nvim/lua/packer_config.lua
+++ common/.config/nvim/lua/packer_config.lua
@@ -30,6 +30,9 @@ local function install_treesitter_parsers()
     -- Neither of these are sync, but that shouldn't matter as there is no
     -- intersection in the parsers they will touch
     vim.cmd("TSUpdate")
+    -- In practice TSInstall may not install new parsers until after NeoVim is
+    -- restarted. I don't consider that a major problem, just means we might lag
+    -- behind a bit.
     vim.cmd("TSInstall all")
 end
 
