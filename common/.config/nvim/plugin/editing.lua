97e1effab69788e1296dd052ad5cca58671cfa0e
Andrew Ferrier <andrewferrier@example.com>
2022-11-05 16:51:55 +0000

    feat: Add linematch option

--- common/.config/nvim/plugin/editing.lua
+++ common/.config/nvim/plugin/editing.lua
@@ -3,3 +3,7 @@ vim.opt.completeopt:remove("menuone")
 vim.opt.completeopt:append("preview") -- Doesn't reliably close
 
 vim.opt.nrformats:append("unsigned")
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+    vim.opt.diffopt:append("linematch:60")
+end
