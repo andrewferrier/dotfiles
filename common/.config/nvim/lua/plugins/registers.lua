b1f85ad7f839e030feb8221d17d04afb251a0493
Andrew Ferrier <andrewferrier@example.com>
2022-11-09 16:59:06 +0000

    refactor: Simplify registers config

--- common/.config/nvim/lua/plugins/registers.lua
+++ common/.config/nvim/lua/plugins/registers.lua
@@ -2,8 +2,6 @@ require("registers").setup({
     show = "*-/#%.0123456789abcdefghijklmnopqrstuvwxyz",
     window = { border = "rounded", transparency = 0 },
     events = {
-        on_register_highlighted = function()
-            -- Do nothing
-        end,
+        on_register_highlighted = false, -- Do nothing
     },
 })
