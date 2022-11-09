aca10604dcdc432fc579219a0ed6d53d97f40cd1
Andrew Ferrier <andrewferrier@example.com>
2022-11-09 12:08:07 +0000

    chore: Simplify configuration that can default

--- common/.config/nvim/lua/plugins/registers.lua
+++ common/.config/nvim/lua/plugins/registers.lua
@@ -1,7 +1,5 @@
 require("registers").setup({
     show = "*-/#%.0123456789abcdefghijklmnopqrstuvwxyz",
-    show_empty = false,
-    trim_whitespace = true,
     window = { border = "rounded", transparency = 0 },
     events = {
         on_register_highlighted = function()
