e2e3b6c69ce3d3640898f266b703572e9182228b
Andrew Ferrier <andrewferrier@example.com>
2022-11-09 17:16:39 +0000

    style: Reformat

--- common/.config/nvim/lua/plugins/fidget.lua
+++ common/.config/nvim/lua/plugins/fidget.lua
@@ -1,11 +1,6 @@
 require("fidget").setup({
-    timer = {
-        fidget_decay = 500,
-        task_decay = 500,
-    },
-    text = {
-        spinner = "star",
-    },
+    timer = { fidget_decay = 500, task_decay = 500 },
+    text = { spinner = "star" },
     fmt = {
         task = function(task_name, _, percentage)
             return string.format(
