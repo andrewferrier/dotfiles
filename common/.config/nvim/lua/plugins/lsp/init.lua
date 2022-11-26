78cdaf6492b57de7852e786db43430e7844b0614
Andrew Ferrier <andrewferrier@example.com>
2022-11-26 21:40:34 +0000

    chore: Remove setting which defaults

--- common/.config/nvim/lua/plugins/lsp/init.lua
+++ common/.config/nvim/lua/plugins/lsp/init.lua
@@ -20,7 +20,6 @@ local sumneko_settings = {
                 library = vim.api.nvim_get_runtime_file("", true),
                 checkThirdParty = false,
             },
-            telemetry = { enable = false },
             -- hint = { enable = true }, -- these hints seem fairly useless for now
         },
     },
