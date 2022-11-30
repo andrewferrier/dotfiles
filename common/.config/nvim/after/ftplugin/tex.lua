5dbfa45939ba1a5355664cc4b0d3d2bab2bedb12
Andrew Ferrier <andrewferrier@example.com>
2022-11-30 13:02:55 +0000

    refactor: Make \cp more robust

--- common/.config/nvim/after/ftplugin/tex.lua
+++ common/.config/nvim/after/ftplugin/tex.lua
@@ -45,26 +45,44 @@ vim.b.minisurround_config = {
 }
 
 local function create_pdf_from_tex(absolute_tex_path)
+    local AUX_DIRECTORY = "-aux-directory=/tmp"
+    local OUTPUT_DIRECTORY = vim.fn.fnamemodify(absolute_tex_path, ":p:h")
+    local OUTPUT_DIRECTORY_ARG = "-output-directory=" .. OUTPUT_DIRECTORY
+
     vim.cmd.update()
     local without_extension = vim.fn.fnamemodify(absolute_tex_path, ":p:r")
-    local directory = vim.fn.fnamemodify(absolute_tex_path, ":p:h")
 
-    local output = vim.fn.system({
-        "latexmk",
-        "-aux-directory=/tmp",
-        "-output-directory=" .. directory,
-        "-pdf",
-        absolute_tex_path,
-    })
+    local done_clean = function(_, exitcode, _)
+        vim.notify("Built, opening...")
 
-    if vim.v.shell_error == 0 then
-        vim.fn.system({ "open", without_extension .. ".pdf" })
-    else
-        vim.notify(
-            "Could not convert to PDF:\n\n" .. output,
-            vim.log.levels.ERROR
-        )
+        if exitcode == 0 then
+            vim.fn.system({ "open", without_extension .. ".pdf" })
+        else
+            vim.notify(
+                "Could not convert to PDF:\n\n" .. exitcode,
+                vim.log.levels.ERROR
+            )
+        end
+
+        vim.notify("Opened, cleaning...")
+        vim.fn.system({ "latexmk", AUX_DIRECTORY, OUTPUT_DIRECTORY_ARG, "-c" })
+        vim.notify("Cleaned.")
     end
+
+    vim.notify("Cleaning...")
+    vim.fn.jobstart({ "latexmk", AUX_DIRECTORY, OUTPUT_DIRECTORY_ARG, "-c" }, {
+        cwd = OUTPUT_DIRECTORY,
+        on_exit = function()
+            vim.notify("Cleaned, building...")
+            vim.fn.jobstart({
+                "latexmk",
+                AUX_DIRECTORY,
+                OUTPUT_DIRECTORY_ARG,
+                "-pdf",
+                absolute_tex_path,
+            }, { on_exit = done_clean, cwd = OUTPUT_DIRECTORY })
+        end,
+    })
 end
 
 vim.keymap.set("n", "<Leader>cp", function()
