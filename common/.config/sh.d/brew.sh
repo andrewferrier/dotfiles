35feb83e4717d7f6eda2737ba27eb780f0d39750
Andrew Ferrier <andrewferrier@example.com>
2023-01-03 13:14:33 +0000

    feat: Add brew-view-installed alias

--- common/.config/sh.d/brew.sh
+++ common/.config/sh.d/brew.sh
@@ -5,4 +5,5 @@ if (command -v brew >/dev/null 2>&1); then
     # shellcheck disable=SC2139
     alias brew-clean-packages="comm -23 <(brew leaves | sort) <(cat ${HOME}/gitco/config/common/pkgs-macos/Brewfile | grep ^brew | cut -d'\"' -f2 | sort) | tac | fzf --multi --preview 'brew info {}' | xargs brew uninstall"
     alias brew-clean-taps="brew tap | fzf -m | xargs brew untap"
+    alias brew-view-installed="(brew list --formula -1 && brew list --casks -1) | sort | fzf --preview 'brew info {}' --reverse"
 fi
