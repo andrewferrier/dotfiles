3c57cec73e18d633549750e5990be60d2aef0ae2
Andrew Ferrier <andrewferrier@example.com>
2023-01-03 09:10:59 +0000

    Revert "chore: Remove detection of background"
    This reverts commit b902d6a79e57a64410466cd3d1b6aed703a87159.
    
    Turns out this is needed! If this isn't present then nested NeoVim
    windows inside a terminal inside NeoVim don't show light color mode when
    they should.

--- common/.config/nvim/lua/plugin-spec/gruvbox.lua
+++ common/.config/nvim/lua/plugin-spec/gruvbox.lua
@@ -1,6 +1,8 @@
 return {
     "ellisonleao/gruvbox.nvim",
     config = function()
+        local statefile = vim.fn.expand("~/.cache/day-night/state-terminal")
+
         local palette = require("gruvbox.palette")
 
         local overrides = {
@@ -15,6 +17,15 @@ return {
             TermCursorNC = { bg = "#00FF00" },
             ["@readonly"] = { bold = true },
         }
+
+        if
+            vim.fn.filereadable(statefile)
+            and vim.fn.index(vim.fn.readfile(statefile), "day") >= 0
+        then
+            vim.opt.background = "light"
+        else
+            vim.opt.background = "dark"
+        end
 
         require("gruvbox").setup({
             contrast = "hard",
