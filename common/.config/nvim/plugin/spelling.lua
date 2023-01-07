6ff0519ff1512459a9e15c0542d62283c662e4da
Andrew Ferrier <andrewferrier@example.com>
2023-01-07 20:01:33 +0000

    feat: Allow for turning off spell with editorconfig

--- common/.config/nvim/plugin/spelling.lua
+++ common/.config/nvim/plugin/spelling.lua
@@ -16,3 +16,13 @@ vim.cmd.iabbrev("reciept", "receipt")
 vim.cmd.iabbrev("reciepts", "receipts")
 vim.cmd.iabbrev("recieve", "receive")
 vim.cmd.iabbrev("recieved", "received")
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+    require("editorconfig").properties.spell = function(bufnr, val, opts)
+        if val == 'true' then
+            vim.opt_local.spell = true
+        elseif val == 'false' then
+            vim.opt_local.spell = false
+        end
+    end
+end
