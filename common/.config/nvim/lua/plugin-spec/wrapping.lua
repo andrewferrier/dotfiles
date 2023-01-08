8f0b693c6a00ea70362dbf51bd13d9ed35f06192
Andrew Ferrier <andrewferrier@example.com>
2023-01-08 12:54:20 +0000

    feat: Add 'help' to wrapping whitelist as an experiment

--- common/.config/nvim/lua/plugin-spec/wrapping.lua
+++ common/.config/nvim/lua/plugin-spec/wrapping.lua
@@ -4,6 +4,17 @@ return {
         require("wrapping").setup({
             softener = { mail = true },
             notify_on_switch = false,
+            auto_set_mode_filetype_allowlist = {
+                "asciidoc",
+                "gitcommit",
+                "help", -- Experiment with adding 'help'
+                "latex",
+                "mail",
+                "markdown",
+                "rst",
+                "tex",
+                "text",
+            },
         })
     end,
 }
