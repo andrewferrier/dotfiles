97151f5e5691dc82db2e78f06b0e406f25c83cff
Andrew Ferrier <andrewferrier@example.com>
2023-01-30 22:13:12 +0000

    feat: Simplify diagnostic signs

--- common/.config/nvim/plugin/diagnostics.lua
+++ common/.config/nvim/plugin/diagnostics.lua
@@ -26,7 +26,7 @@ vim.diagnostic.config({
     },
 })
 
-local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "ℹ" }
+local signs = { Error = "✘", Warn = "!", Hint = "h", Info = "i" }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
