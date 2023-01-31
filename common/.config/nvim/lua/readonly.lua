f48e253a86bbabc7cbee1d099819948f3e37d762
Andrew Ferrier <andrewferrier@example.com>
2023-01-31 14:18:16 +0000

    fix: Don't set syntax highlight for large files

--- common/.config/nvim/lua/readonly.lua
+++ common/.config/nvim/lua/readonly.lua
@@ -3,8 +3,9 @@ local M = {}
 M.make_readonly = function()
     if not vim.b.effective_ro_loaded then
         vim.bo.readonly = true
-        vim.opt_local.conceallevel = 2
+        vim.bo.syntax = ''
         vim.opt_local.concealcursor = "nc"
+        vim.opt_local.conceallevel = 2
         vim.opt_local.list = false
         vim.opt_local.spell = false
 
