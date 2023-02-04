16c9c43c9cadcd7396a6dcb446782999125cc70a
Andrew Ferrier <andrewferrier@example.com>
2023-02-04 00:00:30 +0000

    feat: Add Terraform skeletons

---
+++ common/.config/nvim/skeleton/provider.tf
@@ -0,0 +1,8 @@
+# provider "ibm" {
+#   ibmcloud_api_key = var.IC_API_KEY
+#   region = ""
+# }
+
+# provider "shell" {
+#   interpreter = ["/bin/bash", "-c"]
+# }
---
+++ common/.config/nvim/skeleton/versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    # ibm = {
+    #   source  = "IBM-Cloud/ibm"
+    #   version = ">= 1.50"
+    # }
+    # tls = {
+    #   source = "hashicorp/tls"
+    #   version = ">= 4.0.1"
+    # }
+    # random = {
+    #   source = "hashicorp/random"
+    #   version = ">= 3.3.2"
+    # }
+    # local = {
+    #   source = "hashicorp/local"
+    #   version = ">= 2.2.3"
+    # }
+    # shell = {
+    #   source = "scottwinkler/shell"
+    #   version = ">= 1.7.10"
+    # }
+  }
+}
