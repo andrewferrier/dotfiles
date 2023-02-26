d5ead47d6b75ce8215d72dbf12923589f68fd3f4
Andrew Ferrier <andrewferrier@example.com>
2023-02-26 17:38:59 +0000

    fix: Don't display line numbers for gitcommit

--- common/.config/nvim/after/ftplugin/gitcommit.lua
+++ common/.config/nvim/after/ftplugin/gitcommit.lua
@@ -3,3 +3,6 @@ require("filetype.text").setup()
 if require("large_file").is_large_file() then
     vim.opt_local.spell = false
 end
+
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
