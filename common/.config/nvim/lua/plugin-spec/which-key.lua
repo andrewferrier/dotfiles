00823a34003d1b2982b555776f093af00cc92760
Andrew Ferrier <andrewferrier@example.com>
2023-02-26 08:46:20 +0000

    fix: Don't have which-key conflict with hydra

--- common/.config/nvim/lua/plugin-spec/which-key.lua
+++ common/.config/nvim/lua/plugin-spec/which-key.lua
@@ -1,6 +1,7 @@
 return {
     "folke/which-key.nvim",
+    dependencies = { "anuvyklack/hydra.nvim" },
     config = function()
-        require("which-key").setup({})
+        require("which-key").setup({ triggers_blacklist = { n = { "C-W" } } })
     end,
 }
