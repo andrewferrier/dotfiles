d531a0031aad148afae3668837c1a81c91c92ab5
Andrew Ferrier <andrewferrier@example.com>
2023-02-26 15:23:01 +0000

    fix: Disable ]o, [o to avoid conflicts

--- common/.config/nvim/lua/plugin-spec/wrapping.lua
+++ common/.config/nvim/lua/plugin-spec/wrapping.lua
@@ -2,6 +2,7 @@ return {
     url = "git@github.com:andrewferrier/wrapping.nvim",
     config = function()
         require("wrapping").setup({
+            create_keymaps = false,
             softener = { mail = true },
             notify_on_switch = false,
             auto_set_mode_filetype_allowlist = {
@@ -16,5 +17,9 @@ return {
                 "text",
             },
         })
+
+        vim.keymap.set("n", "yow", function()
+            require('wrapping').toggle_wrap_mode()
+        end, { desc = "Toggle wrap mode" })
     end,
 }
