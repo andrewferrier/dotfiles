a1f4bbc9da2fd011ee4add006db7ef5ddcf45555
Andrew Ferrier <andrewferrier@example.com>
2023-02-26 17:44:13 +0000

    fix: Don't display numbers for git commit

--- common/.config/nvim/plugin/terminal.lua
+++ common/.config/nvim/plugin/terminal.lua
@@ -7,6 +7,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
         vim.wo.relativenumber = false
 
         if vim.fn.has("nvim-0.9.0") == 1 then
+            vim.wo.signcolumn = "no"
             vim.wo.statuscolumn = ""
         end
     end,
