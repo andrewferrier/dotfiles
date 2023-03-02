0ffb87d520d08ad0242bd108699926af64840827
Andrew Ferrier <andrewferrier@example.com>
2023-03-02 17:27:12 +0000

    fix: Extend definition of ansible file

--- common/.config/nvim/plugin/filetype.lua
+++ common/.config/nvim/plugin/filetype.lua
@@ -36,9 +36,9 @@ vim.filetype.add({
         [".*roles/.*%.ya?ml"] = "yaml.ansible",
         [".*tasks/.*%.ya?ml"] = "yaml.ansible",
 
-        [".*hosts.ya?ml"] = "yaml.ansible",
+        [".*hosts.*.ya?ml"] = "yaml.ansible",
         [".*main.ya?ml"] = "yaml.ansible",
-        [".*playbook.ya?ml"] = "yaml.ansible",
+        [".*playbook.*.ya?ml"] = "yaml.ansible",
         [".*site.ya?ml"] = "yaml.ansible",
     },
 })
