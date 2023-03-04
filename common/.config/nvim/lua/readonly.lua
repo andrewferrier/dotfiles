5ab2df6a9be3bcd5d3d9e18dfe8c63a7eb96a7fe
Andrew Ferrier <andrewferrier@example.com>
2023-03-04 20:18:47 +0000

    chore: Make readonly idempotent

--- common/.config/nvim/lua/readonly.lua
+++ common/.config/nvim/lua/readonly.lua
@@ -1,23 +1,19 @@
 local M = {}
 
 M.make_readonly = function()
-    if not vim.b.effective_ro_loaded then
-        vim.bo.readonly = true
-        vim.bo.syntax = ""
-        vim.opt_local.concealcursor = "nc"
-        vim.opt_local.conceallevel = 2
-        vim.opt_local.list = false
-        vim.opt_local.spell = false
+    vim.bo.readonly = true
+    vim.bo.syntax = ""
+    vim.opt_local.concealcursor = "nc"
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.list = false
+    vim.opt_local.spell = false
 
-        require("filetype.status_column").remove_signs()
-        require("diagnostics").hide()
-        require("gitsigns").detach()
+    require("filetype.status_column").remove_signs()
+    require("diagnostics").hide()
+    require("gitsigns").detach()
 
-        if vim.b.text_based_filetype then
-            require("wrapping").soft_wrap_mode()
-        end
-
-        vim.b.effective_ro_loaded = true
+    if vim.b.text_based_filetype then
+        require("wrapping").soft_wrap_mode()
     end
 end
 
