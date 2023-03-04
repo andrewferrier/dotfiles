eedb7d83597c53ff5d3e0967784850c905d65884
Andrew Ferrier <andrewferrier@example.com>
2023-03-04 20:18:16 +0000

    chore: Remove unused <CR> shortcut

--- common/.config/nvim/plugin/rename-file.lua
+++ common/.config/nvim/plugin/rename-file.lua
@@ -5,11 +5,3 @@ vim.keymap.set("n", "<Leader>r", function()
         return ':Rename <C-R>=expand("%:t")<CR>'
     end
 end, { expr = true })
-
-vim.keymap.set("n", "<CR>", function()
-    if vim.o.buftype == "" and vim.o.modified == true then
-        return ":update<CR>"
-    else
-        return "<CR>"
-    end
-end, { expr = true })
