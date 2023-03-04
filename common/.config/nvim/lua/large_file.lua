35e51fb987f70d4aed2b2ea1c20d6a3e249206d0
Andrew Ferrier <andrewferrier@example.com>
2023-03-04 20:19:01 +0000

    refactor: Simplify large_file

--- common/.config/nvim/lua/large_file.lua
+++ common/.config/nvim/lua/large_file.lua
@@ -3,11 +3,7 @@ local M = {}
 M.LARGE_FILE_LINE_COUNT = 10000
 
 M.is_large_file = function(bufnr)
-    if bufnr == nil then
-        bufnr = 0
-    end
-
-    return (vim.api.nvim_buf_line_count(bufnr) > M.LARGE_FILE_LINE_COUNT)
+    return vim.api.nvim_buf_line_count(bufnr or 0) > M.LARGE_FILE_LINE_COUNT
 end
 
 return M
