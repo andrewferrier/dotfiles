fd47a104821ea942218993c616709a73890d90ed
Andrew Ferrier <andrewferrier@example.com>
2023-03-04 21:59:44 +0000

    perf: Lazy-load parts of mini.lua

--- common/.config/nvim/lua/plugin-spec/mini.lua
+++ common/.config/nvim/lua/plugin-spec/mini.lua
@@ -1,15 +1,6 @@
 return {
     "echasnovski/mini.nvim",
     config = function()
-        require("mini.ai").setup()
-
-        require("mini.align").setup({
-            mappings = {
-                start = nil,
-                start_with_preview = "gl",
-            },
-        })
-
         vim.g.minibasics_silence = true
 
         require("mini.basics").setup({
@@ -17,25 +8,40 @@ return {
             mappings = { basic = false, option_toggle_prefix = "yo" },
         })
 
-        -- Delete option-toggling prefixes I don't use to avoid accidentally
-        -- turning them off
-        vim.keymap.del("n", "yoh")
-        vim.keymap.del("n", "yoi")
-
-        require("mini.bracketed").setup({
-            comment = { suffix = "o" },
-            indent = { options = { change_type = "diff" } },
-            jump = { suffix = "" },
-            oldfile = { suffix = "" },
-            window = { suffix = "" },
-            undo = { suffix = "" }, -- Undo causes a lot of flicker with cmdheight=0
-        })
-
-        require("mini.comment").setup({})
-
         -- Will be replaced with https://github.com/neovim/neovim/issues/16339
         require("mini.misc").setup_restore_cursor()
 
-        require("mini.surround").setup({})
+        vim.api.nvim_create_autocmd({ "BufEnter" }, {
+            group = vim.api.nvim_create_augroup("BufEnter_mini.lua", {}),
+            once = true,
+            callback = function()
+                require("mini.ai").setup()
+
+                require("mini.align").setup({
+                    mappings = {
+                        start = nil,
+                        start_with_preview = "gl",
+                    },
+                })
+
+                -- Delete option-toggling prefixes I don't use to avoid accidentally
+                -- turning them off
+                vim.keymap.del("n", "yoh")
+                vim.keymap.del("n", "yoi")
+
+                require("mini.bracketed").setup({
+                    comment = { suffix = "o" },
+                    indent = { options = { change_type = "diff" } },
+                    jump = { suffix = "" },
+                    oldfile = { suffix = "" },
+                    window = { suffix = "" },
+                    undo = { suffix = "" }, -- Undo causes a lot of flicker with cmdheight=0
+                })
+
+                require("mini.comment").setup({})
+
+                require("mini.surround").setup({})
+            end,
+        })
     end,
 }
