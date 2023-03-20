9e7140b6e8d37e5aa97ce4f7200d809a0ddce4b2
Andrew Ferrier <andrewferrier@example.com>
2023-03-20 13:45:30 +0000

    refactor: Move function inside guard

--- common/.config/nvim/plugin/semantic-highlighting-readonly-error.lua
+++ common/.config/nvim/plugin/semantic-highlighting-readonly-error.lua
@@ -1,33 +1,33 @@
 -- Based on
 -- https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316#complex-highlighting
 
-local function show_unconst_caps(args)
-    local token = args.data.token
-    if token.type ~= "variable" or token.modifiers.readonly then
-        return
-    end
-
-    local text = vim.api.nvim_buf_get_text(
-        args.buf,
-        token.line,
-        token.start_col,
-        token.line,
-        token.end_col,
-        {}
-    )[1]
-    if text ~= string.upper(text) then
-        return
-    end
-
-    vim.lsp.semantic_tokens.highlight_token(
-        token,
-        args.buf,
-        args.data.client_id,
-        "Error"
-    )
-end
-
 if vim.fn.has("nvim-0.9.0") == 1 then
+    local function show_unconst_caps(args)
+        local token = args.data.token
+        if token.type ~= "variable" or token.modifiers.readonly then
+            return
+        end
+
+        local text = vim.api.nvim_buf_get_text(
+            args.buf,
+            token.line,
+            token.start_col,
+            token.line,
+            token.end_col,
+            {}
+        )[1]
+        if text ~= string.upper(text) then
+            return
+        end
+
+        vim.lsp.semantic_tokens.highlight_token(
+            token,
+            args.buf,
+            args.data.client_id,
+            "Error"
+        )
+    end
+
     vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
             local filetype = vim.api.nvim_buf_get_option(args.buf, "filetype")
