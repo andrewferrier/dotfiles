15b7b6a741dba7e1a08be12fd44074d1fac387be
Andrew Ferrier <andrewferrier@example.com>
2023-03-20 14:19:33 +0000

    refactor: Remove over-optimization

--- common/.config/nvim/plugin/filetype.lua
+++ common/.config/nvim/plugin/filetype.lua
@@ -1,52 +1,44 @@
-local configure = function()
-    vim.filetype.add({
-        extension = {
-            equivs = "debcontrol",
-            jad = "java",
-            rasi = "rasi",
-            tfstate = "json",
-        },
-        filename = {
-            [".ansible-lint"] = "yaml",
-            [".dockerignore"] = "gitignore",
-            [".gitlint"] = "cfg",
-            [".sqlfluff"] = "cfg",
-            ["control_template"] = "debcontrol",
-            ["shellcheckrc"] = "conf",
-        },
-        pattern = {
-            ["%.secrets.*"] = "sh",
-            [".*%.gitignore.*"] = "gitignore",
-            [".*%.properties.*"] = "jproperties",
-            [".*/%.kaf/config"] = "yaml",
-            [".*Dockerfile.*"] = "dockerfile",
-            [".*Jenkinsfile.*"] = "groovy",
-            [".*emails/cur/.*"] = "mail",
-            [".*emails/new/.*"] = "mail",
-            [".*emails/tmp/.*"] = "mail",
-            [".*envrc.*"] = "sh",
-            [".*mkd%.txt"] = "markdown",
-            [".*my%-ublock.*"] = "json",
-            ["muttrc%..*"] = "muttrc",
+vim.filetype.add({
+    extension = {
+        equivs = "debcontrol",
+        jad = "java",
+        rasi = "rasi",
+        tfstate = "json",
+    },
+    filename = {
+        [".ansible-lint"] = "yaml",
+        [".dockerignore"] = "gitignore",
+        [".gitlint"] = "cfg",
+        [".sqlfluff"] = "cfg",
+        ["control_template"] = "debcontrol",
+        ["shellcheckrc"] = "conf",
+    },
+    pattern = {
+        ["%.secrets.*"] = "sh",
+        [".*%.gitignore.*"] = "gitignore",
+        [".*%.properties.*"] = "jproperties",
+        [".*/%.kaf/config"] = "yaml",
+        [".*Dockerfile.*"] = "dockerfile",
+        [".*Jenkinsfile.*"] = "groovy",
+        [".*emails/cur/.*"] = "mail",
+        [".*emails/new/.*"] = "mail",
+        [".*emails/tmp/.*"] = "mail",
+        [".*envrc.*"] = "sh",
+        [".*mkd%.txt"] = "markdown",
+        [".*my%-ublock.*"] = "json",
+        ["muttrc%..*"] = "muttrc",
 
-            -- Similar logic to pearofducks/ansible-vim
-            [".*group_vars/.*"] = "yaml.ansible",
-            [".*host_vars/.*"] = "yaml.ansible",
+        -- Similar logic to pearofducks/ansible-vim
+        [".*group_vars/.*"] = "yaml.ansible",
+        [".*host_vars/.*"] = "yaml.ansible",
 
-            [".*handlers/.*%.ya?ml"] = "yaml.ansible",
-            [".*roles/.*%.ya?ml"] = "yaml.ansible",
-            [".*tasks/.*%.ya?ml"] = "yaml.ansible",
+        [".*handlers/.*%.ya?ml"] = "yaml.ansible",
+        [".*roles/.*%.ya?ml"] = "yaml.ansible",
+        [".*tasks/.*%.ya?ml"] = "yaml.ansible",
 
-            [".*hosts.*.ya?ml"] = "yaml.ansible",
-            [".*main.ya?ml"] = "yaml.ansible",
-            [".*playbook.*.ya?ml"] = "yaml.ansible",
-            [".*site.ya?ml"] = "yaml.ansible",
-        },
-    })
-end
-
-vim.api.nvim_create_autocmd({ "User" }, {
-    pattern = "VeryLazy",
-    group = vim.api.nvim_create_augroup("VeryLazy_filetypes", {}),
-    callback = configure,
+        [".*hosts.*.ya?ml"] = "yaml.ansible",
+        [".*main.ya?ml"] = "yaml.ansible",
+        [".*playbook.*.ya?ml"] = "yaml.ansible",
+        [".*site.ya?ml"] = "yaml.ansible",
+    },
 })
