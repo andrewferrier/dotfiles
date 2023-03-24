e019c3549fafe8d238c8848353d220a97eed5566
Andrew Ferrier <andrewferrier@example.com>
2023-03-24 18:19:26 +0000

    fix: Add dependency on gruvbox

--- common/.config/nvim/lua/plugin-spec/tint.lua
+++ common/.config/nvim/lua/plugin-spec/tint.lua
@@ -1,5 +1,6 @@
 return {
     "levouh/tint.nvim",
+    dependencies = { "ellisonleao/gruvbox.nvim" },
     config = function()
         if vim.o.background == "light" then
             require("tint").setup({ tint = 55, saturation = 0.4 })
