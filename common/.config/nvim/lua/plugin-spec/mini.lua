2d2f313ac9b5c621d81f6858a5017df044516cbf
Andrew Ferrier <andrewferrier@example.com>
2023-03-24 21:47:00 +0000

    refactor: Simplify mini.nvim config

--- common/.config/nvim/lua/plugin-spec/mini.lua
+++ common/.config/nvim/lua/plugin-spec/mini.lua
@@ -1,6 +1,15 @@
 return {
     "echasnovski/mini.nvim",
     config = function()
+        require("mini.ai").setup()
+
+        require("mini.align").setup({
+            mappings = {
+                start = nil,
+                start_with_preview = "gl",
+            },
+        })
+
         require("mini.basics").setup({
             silent = true,
             options = { basic = false },
@@ -12,37 +21,22 @@ return {
         vim.keymap.del("n", "yoh")
         vim.keymap.del("n", "yoi")
 
+        require("mini.bracketed").setup({
+            comment = { suffix = "o" }, -- 'c' is used in diff mode for diffs
+            indent = { options = { change_type = "diff" } },
+            jump = { suffix = "" },
+            oldfile = { suffix = "" },
+            window = { suffix = "" },
+            undo = { suffix = "" }, -- Undo causes a lot of flicker with cmdheight=0
+        })
+
+        require("mini.comment").setup()
+
         -- Will be replaced with https://github.com/neovim/neovim/issues/16339
         require("mini.misc").setup_restore_cursor()
 
-        vim.api.nvim_create_autocmd({ "BufEnter" }, {
-            group = vim.api.nvim_create_augroup("BufEnter_mini.lua", {}),
-            once = true,
-            callback = function()
-                require("mini.ai").setup()
-
-                require("mini.align").setup({
-                    mappings = {
-                        start = nil,
-                        start_with_preview = "gl",
-                    },
-                })
-
-                require("mini.bracketed").setup({
-                    comment = { suffix = "o" }, -- 'c' is used in diff mode for diffs
-                    indent = { options = { change_type = "diff" } },
-                    jump = { suffix = "" },
-                    oldfile = { suffix = "" },
-                    window = { suffix = "" },
-                    undo = { suffix = "" }, -- Undo causes a lot of flicker with cmdheight=0
-                })
-
-                require("mini.comment").setup({})
-
-                require("mini.surround").setup({
-                    mappings = { find = "", find_left = "", highlight = "" },
-                })
-            end,
+        require("mini.surround").setup({
+            mappings = { find = "", find_left = "", highlight = "" },
         })
     end,
 }
