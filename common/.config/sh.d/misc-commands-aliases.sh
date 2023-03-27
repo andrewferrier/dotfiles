c30c984c72b812cebf3e3837ce1fe7ee1310a523
Andrew Ferrier <andrewferrier@example.com>
2023-03-27 19:45:52 +0100

    refactor: Move aliases from gitco

--- common/.config/sh.d/misc-commands-aliases.sh
+++ common/.config/sh.d/misc-commands-aliases.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 alias docker-cleanup="docker container prune -f ; docker image prune -f ; docker volume prune -f ; docker network prune -f"
+alias rm-broken-links='find . -xtype l | fzf -m | xargs rm'
+alias webshare='python3 -m http.server'
 alias docker-rmi-interactive="docker images | sed -E 's%([^[:space:]]+)[[:space:]]+([^[:space:]]+)[[:space:]]+([^[:space:]]+).*%\\1:\\2 \\3%g' | tail -n +2 | sort -r | fzf -d' ' -m --with-nth=1 | cut -d' ' -f2 | xargs -t docker image rm"
 alias external-ip="curl -4 ifconfig.co"
 alias icat='kitty +kitten icat'
+alias socks-ssh-setup='ssh -f -N -D 1080 '
 alias speedtest="docker run --rm --net=host docker.io/tianon/speedtest --accept-license --accept-gdpr"
 alias testssl='docker run -t --rm docker.io/mvance/testssl'
 
@@ -11,6 +14,7 @@ if [[ ${OSTYPE} == darwin* ]]; then
     alias listening="sudo lsof -iTCP -sTCP:LISTEN -n -P"
 else
     alias listening='sudo ss --listening --tcp --udp --numeric --process'
+    alias monitor-wifi='watch "nmcli --colors yes dev wifi list"'
     alias sparseness="find . -type f -printf '%S\t%p\n' | column -t -s $'\t' | sort -n -r"
 fi
 
