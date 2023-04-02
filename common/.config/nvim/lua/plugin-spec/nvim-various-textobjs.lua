7cb56f2ae2d09bdbd49965f5900ac6e771d8dbdd
Andrew Ferrier <andrewferrier@example.com>
2023-04-02 17:21:08 +0100

    refactor: Use vimscript for textobjects
    (Allows for dot-repeat)

--- common/.config/nvim/lua/plugin-spec/nvim-various-textobjs.lua
+++ common/.config/nvim/lua/plugin-spec/nvim-various-textobjs.lua
@@ -1,27 +1,48 @@
 return {
     "chrisgrieser/nvim-various-textobjs",
     config = function()
-        vim.keymap.set({ "o", "x" }, "ak", function()
-            require("various-textobjs").key(false)
-        end, { desc = "'key'/lhs including assignment" })
-        vim.keymap.set({ "o", "x" }, "ik", function()
-            require("various-textobjs").key(true)
-        end, { desc = "'key'/lhs" })
-        vim.keymap.set({ "o", "x" }, "av", function()
-            require("various-textobjs").value(false)
-        end, { desc = "'value'/rhs including terminator" })
-        vim.keymap.set({ "o", "x" }, "iv", function()
-            require("various-textobjs").value(true)
-        end, { desc = "'value'/rhs" })
-        vim.keymap.set({ "o", "x" }, "aS", function()
-            require("various-textobjs").subword(false)
-        end, { desc = "SubWord" })
-        vim.keymap.set({ "o", "x" }, "iS", function()
-            require("various-textobjs").subword(true)
-        end, { desc = "SubWord" })
-        vim.keymap.set({ "o", "x" }, "id", function()
-            require("various-textobjs").diagnostic()
-        end, { desc = "diagnostic" })
+        vim.keymap.set(
+            { "o", "x" },
+            "ak",
+            '<cmd>lua require("various-textobjs").key(false)<CR>',
+            { desc = "'key'/lhs including assignment" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "ik",
+            '<cmd>lua require("various-textobjs").key(true)<CR>',
+            { desc = "'key'/lhs" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "av",
+            '<cmd>lua require("various-textobjs").value(false)<CR>',
+            { desc = "'value'/rhs including terminator" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "iv",
+            '<cmd>lua require("various-textobjs").value(true)<CR>',
+            { desc = "'value'/rhs" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "aS",
+            '<cmd>lua require("various-textobjs").subword(false)<CR>',
+            { desc = "SubWord" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "iS",
+            '<cmd>lua require("various-textobjs").subword(true)<CR>',
+            { desc = "SubWord" }
+        )
+        vim.keymap.set(
+            { "o", "x" },
+            "id",
+            '<cmd>lua require("various-textobjs").diagnostic()<CR>',
+            { desc = "diagnostic" }
+        )
     end,
     event = "BufEnter",
 }
