fc4d993054c6521843318a6a6f5661222ca55cc5
Andrew Ferrier <andrewferrier@example.com>
2023-04-03 09:35:12 +0100

    chore: Remove auto-readonly logic
    I basically don't use/need it.

--- common/.config/nvim/plugin/readonly.lua
+++ common/.config/nvim/plugin/readonly.lua
@@ -1,27 +1,4 @@
-local EXCLUDED_DIRS = { vim.env.HOME, "/private/tmp", "/private/var", "/tmp" }
-
-local function is_excluded(bufname, dirs)
-    for _, val in pairs(dirs) do
-        if string.sub(bufname, 1, string.len(val)) == val then
-            return true
-        end
-    end
-
-    return false
-end
-
 vim.keymap.set("n", "cvr", function()
     require("readonly").make_readonly()
     vim.notify("Effective R/O on.")
 end, { desc = "Turn on effective r/o" })
-
-vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-    desc = "Make files readonly when outside of home directory",
-    pattern = "*",
-    group = vim.api.nvim_create_augroup("BufReadPost_readonly.lua", {}),
-    callback = function()
-        if not is_excluded(vim.api.nvim_buf_get_name(0), EXCLUDED_DIRS) then
-            require("readonly").make_readonly()
-        end
-    end,
-})
