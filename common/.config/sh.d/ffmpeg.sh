e987aa4a9061e4b92364e2c896a9b064ee643ecf
Andrew Ferrier <andrewferrier@example.com>
2023-04-04 23:10:49 +0100

    fix: Warn if input files not specified

--- common/.config/sh.d/ffmpeg.sh
+++ common/.config/sh.d/ffmpeg.sh
@@ -2,14 +2,14 @@
 
 if (command -v ffmpeg >/dev/null 2>&1); then
     function ffmpeg-to-audioonly-lowres {
-        INPUT=$1
+        INPUT=${1?Specify input file}
         OUTPUT="${2:-${INPUT%.*}-audioonly-lowres.mp3}"
 
         ffmpeg -i "${INPUT}" -vn -codec:a libmp3lame -q:a 7 "${OUTPUT}"
     }
 
     function ffmpeg-to-all-lowres {
-        INPUT=$1
+        INPUT=${1?Specify input file}
         OUTPUT="${2:-${INPUT%.*}-lowres.mp4}"
 
         ffmpeg -i "${INPUT}" -filter:v scale="trunc(oh*a/2)*2:480" -c:v libx265 -crf 28 -codec:a libmp3lame -q:a 7 "${OUTPUT}"
