d0503a1d44e8a4f7ed40451018f0305131cbdc66
Andrew Ferrier <andrewferrier@example.com>
2023-04-04 18:05:47 +0100

    feat: Add ffmpeg-to-all-lowres func

--- common/.config/sh.d/ffmpeg.sh
+++ common/.config/sh.d/ffmpeg.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 if (command -v ffmpeg >/dev/null 2>&1); then
-    function ffmpeg-extract-audio-lowres {
+    function ffmpeg-to-audioonly-lowres {
         INPUT=$1
-        OUTPUT="${2:-${INPUT%.mkv}.mp3}"
+        OUTPUT="${2:-${INPUT%.*}-audioonly-lowres.mp3}"
 
-        ffmpeg -i "${INPUT}" -vn -codec:a libmp3lame -qscale:a 7 "${OUTPUT}"
+        ffmpeg -i "${INPUT}" -vn -codec:a libmp3lame -q:a 7 "${OUTPUT}"
     }
+
+    function ffmpeg-to-all-lowres {
+        INPUT=$1
+        OUTPUT="${2:-${INPUT%.*}-lowres.mp4}"
+
+        ffmpeg -i "${INPUT}" -filter:v scale="trunc(oh*a/2)*2:480" -c:v libx265 -crf 28 -codec:a libmp3lame -q:a 7 "${OUTPUT}"
+}
 fi
