af205eee7bea9eaf5c230aef46432ad04c5e4217
Andrew Ferrier <andrewferrier@example.com>
2023-04-04 15:14:50 +0100

    fix: Allow multiple top-level headers

--- if-command/mdl/.config/mdl/.mdl.rb
+++ if-command/mdl/.config/mdl/.mdl.rb
@@ -8,5 +8,6 @@ rule 'MD046', :style => :consistent
 exclude_rule 'MD009' # use null_ls instead, it's more accurate
 exclude_rule 'MD013'
 exclude_rule 'MD024' # it is valid to have multiple headers with the same content in multiple sections!
+exclude_rule 'MD025' # in documents with frontmatter, multiple top-level headers are fine
 exclude_rule 'MD041' # it is not necessarily true that the first line should be top-level, e.g. frontmatter
 exclude_rule 'MD033'
