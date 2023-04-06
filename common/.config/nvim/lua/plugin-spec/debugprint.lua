97cec749797f111553eddf3bb333ee9601c6801d
Andrew Ferrier <andrewferrier@example.com>
2023-04-06 17:34:51 +0100

    fix: Remove treesitter dep on 0.9+

--- common/.config/nvim/lua/plugin-spec/debugprint.lua
+++ common/.config/nvim/lua/plugin-spec/debugprint.lua
@@ -1,6 +1,11 @@
-return {
+local spec = {
     url = "git@github.com:andrewferrier/debugprint.nvim",
     opts = {},
-    dependencies = "nvim-treesitter/nvim-treesitter",
     -- Don't use 'keys'; won't work if visual mode is used first
 }
+
+if vim.fn.has("nvim-0.9.0") ~= 1 then
+    spec.dependencies = "nvim-treesitter/nvim-treesitter"
+end
+
+return spec
