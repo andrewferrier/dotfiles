eb16c9a744acd371ec1cf255236f003e4e03bdaf
Andrew Ferrier <andrewferrier@example.com>
2023-04-08 00:49:18 +0100

    fix: Don't display filename if empty

--- common/.config/nvim/lua/plugin-spec/nvim-notify.lua
+++ common/.config/nvim/lua/plugin-spec/nvim-notify.lua
@@ -17,7 +17,14 @@ return {
             msg = vim.trim(msg)
 
             if msg ~= "" then
-                msg = " " .. vim.fn.expand("%:t") .. ": " .. msg
+                local filename = vim.trim(vim.fn.expand("%:t"))
+
+                if filename ~= "" then
+                    filename = filename .. ": "
+                end
+
+                msg = " " .. filename .. msg
+
                 require("notify")(msg, level, opts)
             end
         end
