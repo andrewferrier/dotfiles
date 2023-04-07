f565b3ee2fe61e764e98894d1067fd427cec85a7
Andrew Ferrier <andrewferrier@example.com>
2023-04-07 18:14:45 +0100

    feat: Reduce saturation

--- common/.config/nvim/lua/plugin-spec/tint.lua
+++ common/.config/nvim/lua/plugin-spec/tint.lua
@@ -3,9 +3,9 @@ return {
     dependencies = { "ellisonleao/gruvbox.nvim" },
     config = function()
         if vim.o.background == "light" then
-            require("tint").setup({ tint = 55, saturation = 0.4 })
+            require("tint").setup({ tint = 55, saturation = 0.3 })
         else
-            require("tint").setup({ saturation = 0.4 })
+            require("tint").setup({ saturation = 0.3 })
         end
     end,
     event = "VeryLazy",
