79ce1469f3249750d1c4eda83b9abacf5eb9d2ef
Andrew Ferrier <andrewferrier@example.com>
2023-04-14 12:07:01 +0100

    refactor: Improve readability of less.sh

--- common/.config/sh-profile.d/less.sh
+++ common/.config/sh-profile.d/less.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-LESS_TERMCAP_md="$(tput bold; tput setaf 4)"
-LESS_TERMCAP_me="$(tput sgr0)"
-LESS_TERMCAP_mb="$(tput blink)"
-LESS_TERMCAP_us="$(tput setaf 2)"
-LESS_TERMCAP_ue="$(tput sgr0)"
-LESS_TERMCAP_so="$(tput smso)"
-LESS_TERMCAP_se="$(tput rmso)"
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+BLINK=$(tput blink)
+GREEN=$(tput setaf 2)
+REVERSE=$(tput smso)
+NORMAL=$(tput rmso)
 
-export LESS_TERMCAP_md LESS_TERMCAP_me LESS_TERMCAP_mb LESS_TERMCAP_us LESS_TERMCAP_ue LESS_TERMCAP_so LESS_TERMCAP_se
+export LESS_TERMCAP_md="$BOLD$GREEN"
+export LESS_TERMCAP_me="$RESET"
+export LESS_TERMCAP_mb="$BOLD$BLINK"
+export LESS_TERMCAP_us="$GREEN"
+export LESS_TERMCAP_ue="$RESET"
+export LESS_TERMCAP_so="$REVERSE"
+export LESS_TERMCAP_se="$NORMAL"
 
 export PAGER=less
