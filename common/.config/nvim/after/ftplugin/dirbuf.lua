15ed9cf70d6eec10bdc0e512880021b1ce3b0d90
Andrew Ferrier <andrewferrier@example.com>
2023-04-14 15:52:52 +0100

    chore: Remove redundant diagnostic disable

--- common/.config/nvim/after/ftplugin/dirbuf.lua
+++ common/.config/nvim/after/ftplugin/dirbuf.lua
@@ -1,8 +1,6 @@
 vim.opt_local.listchars:remove("tab:>·")
 vim.opt_local.listchars:append("tab:  ")
 
-vim.diagnostic.disable(0)
-
 -- These keybindings are intentionally similar to fzf
 vim.keymap.set("n", "<C-S>", function()
     require("dirbuf").enter("split")
