dd72d443486a87468cfbebeec3e9640c45e8b131
Andrew Ferrier <andrewferrier@example.com>
2023-04-19 17:01:13 +0100

    feat: Move to appropriate indent with 'i'

--- common/.config/nvim/plugin/editing.lua
+++ common/.config/nvim/plugin/editing.lua
@@ -23,3 +23,15 @@ vim.keymap.set(
     "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>",
     { desc = "Put empty line(s) below" }
 )
+
+-- Move to appropriate indent on 'i'. See
+-- https://www.reddit.com/r/neovim/comments/12rqyl8/comment/jgvs6im/?utm_source=share&utm_medium=web2x&context=3
+local move_to_indent = function()
+    if #vim.fn.getline(".") == 0 then
+        return [["_cc]]
+    else
+        return "i"
+    end
+end
+
+vim.keymap.set("n", "i", move_to_indent, { expr = true })
