6bdea78b4043b0a5308933a1b548efcf38d48d3c
Andrew Ferrier <andrewferrier@example.com>
2023-05-14 10:31:56 +0100

    fix: Issue with antidote not loading correctly

--- if-command/zsh/.config/zsh.d/50-load-plugins.zsh
+++ if-command/zsh/.config/zsh.d/50-load-plugins.zsh
@@ -1,3 +1,2 @@
+source ${ZSH_CACHE_FOLDER}/antidote/antidote.zsh
 source ${ZSH_CACHE_FOLDER}/plugins.sh
-# Support commands like `antidote update`
-autoload -Uz ${ZSH_CACHE_FOLDER}/antidote/functions/*
