ca0b93e4f112943ca2703a59abf9fb9bd706d639
Andrew Ferrier <andrew@example.com>
2023-05-27 20:38:02 +0000

    fix: Check for existence of tput

--- common/.config/sh-profile.d/less.sh
+++ common/.config/sh-profile.d/less.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-BOLD=$(tput bold)
-RESET=$(tput sgr0)
-BLINK=$(tput blink)
-GREEN=$(tput setaf 2)
-REVERSE=$(tput smso)
-NORMAL=$(tput rmso)
+if type tput > /dev/null 2> /dev/null; then
+    BOLD=$(tput bold)
+    RESET=$(tput sgr0)
+    BLINK=$(tput blink)
+    GREEN=$(tput setaf 2)
+    REVERSE=$(tput smso)
+    NORMAL=$(tput rmso)
 
-export LESS_TERMCAP_md="$BOLD$GREEN"
-export LESS_TERMCAP_me="$RESET"
-export LESS_TERMCAP_mb="$BOLD$BLINK"
-export LESS_TERMCAP_us="$GREEN"
-export LESS_TERMCAP_ue="$RESET"
-export LESS_TERMCAP_so="$REVERSE"
-export LESS_TERMCAP_se="$NORMAL"
+    export LESS_TERMCAP_md="$BOLD$GREEN"
+    export LESS_TERMCAP_me="$RESET"
+    export LESS_TERMCAP_mb="$BOLD$BLINK"
+    export LESS_TERMCAP_us="$GREEN"
+    export LESS_TERMCAP_ue="$RESET"
+    export LESS_TERMCAP_so="$REVERSE"
+    export LESS_TERMCAP_se="$NORMAL"
+fi
 
 export PAGER=less
