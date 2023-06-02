7b78c70b62f34f9ba8af3d60cde693a3d419c4c6
Andrew Ferrier <andrewferrier@example.com>
2023-06-02 20:27:14 +0100

    fix: Check if fasd installed

--- if-command/zsh/.config/zsh-profile.d/prep-fasd.zsh
+++ if-command/zsh/.config/zsh-profile.d/prep-fasd.zsh
@@ -1,2 +1,6 @@
-fasd --init zsh-hook > $ZSH_CACHE_FOLDER/fasd-zsh-hook.zsh
-chmod +x $ZSH_CACHE_FOLDER/fasd-zsh-hook.zsh
+if type fasd >/dev/null 2>/dev/null; then
+    fasd --init zsh-hook > $ZSH_CACHE_FOLDER/fasd-zsh-hook.zsh
+    chmod +x $ZSH_CACHE_FOLDER/fasd-zsh-hook.zsh
+else
+    >&2 echo 'WARNING: fasd not installed.'
+fi
