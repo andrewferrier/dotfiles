d816e2720bfe3ea78f4c2386e25d832e1867786e
Andrew Ferrier <andrewferrier@example.com>
2023-07-28 17:16:41 +0100

    fix: Use NeoVim only if installed

--- stow/common/.config/sh-profile.d/editor.sh
+++ stow/common/.config/sh-profile.d/editor.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Some commands (e.g. abcde) don't use VISUAL properly, so EDITOR is needed.
-VISUAL=$(command -v nvim)
+
+if command -v nvim >/dev/null 2>/dev/null; then
+    VISUAL=$(command -v nvim)
+else
+    VISUAL=$(command -v vim)
+fi
+
 export VISUAL
 EDITOR=${VISUAL}
 export EDITOR
