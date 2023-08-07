02eb1ca257c3c376c305acbddf73def124541085
Andrew Ferrier <andrewferrier@example.com>
2023-08-07 20:42:12 +0100

    chore: Inline overrides and remove LspInlayHint

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
@@ -13,25 +13,22 @@ return {
             vim.opt.background = "dark"
         end
 
-        local overrides = {
-            TermCursorNC = { bg = "#00DD00" },
-            LspInlayHint = { link = 'StatusLineNC' },
-
-            -- See https://github.com/ellisonleao/gruvbox.nvim/issues/230#issuecomment-1493883602
-            GruvboxAquaSign = { bg = "" },
-            GruvboxBlueSign = { bg = "" },
-            GruvboxGreenSign = { bg = "" },
-            GruvboxOrangeSign = { bg = "" },
-            GruvboxPurpleSign = { bg = "" },
-            GruvboxRedSign = { bg = "" },
-            GruvboxYellowSign = { bg = "" },
-
-            ["@lsp.mod.readonly"] = { bold = true },
-        }
-
         require("gruvbox").setup({
             contrast = "hard",
-            overrides = overrides,
+            overrides = {
+                TermCursorNC = { bg = "#00DD00" },
+
+                -- See https://github.com/ellisonleao/gruvbox.nvim/issues/230#issuecomment-1493883602
+                GruvboxAquaSign = { bg = "" },
+                GruvboxBlueSign = { bg = "" },
+                GruvboxGreenSign = { bg = "" },
+                GruvboxOrangeSign = { bg = "" },
+                GruvboxPurpleSign = { bg = "" },
+                GruvboxRedSign = { bg = "" },
+                GruvboxYellowSign = { bg = "" },
+
+                ["@lsp.mod.readonly"] = { bold = true },
+            },
             italic = {
                 strings = false,
                 operators = false,
