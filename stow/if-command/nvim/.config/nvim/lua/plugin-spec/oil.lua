72ad77191b4f008a09b92a9b574ca6cbddc200d2
Andrew Ferrier <andrewferrier@example.com>
2023-08-20 22:18:53 +0100

    fix: No warnings if trash-put not available

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/oil.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/oil.lua
@@ -1,12 +1,22 @@
 return {
     "stevearc/oil.nvim",
     config = function()
-        require("oil").setup({
-            delete_to_trash = true,
+        local opts = {
             skip_confirm_for_simple_edits = true,
             view_options = { show_hidden = true },
-        })
+        }
 
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        if not vim.fn.executable('trash-put') then
+            opts.delete_to_trash = false
+        end
+
+        require("oil").setup(opts)
+
+        vim.keymap.set(
+            "n",
+            "-",
+            "<CMD>Oil<CR>",
+            { desc = "Open parent directory" }
+        )
     end,
 }
