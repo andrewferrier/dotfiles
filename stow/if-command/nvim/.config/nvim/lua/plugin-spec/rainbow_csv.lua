8a54507859ae76b0cae870ad4a5ff636febbda0f
Andrew Ferrier <andrewferrier@example.com>
2023-08-24 18:55:24 +0100

    chore: Remove unused plugins

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/rainbow_csv.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "mechatroner/rainbow_csv",
-    ft = { "csv", "tsv" },
-    config = function()
-        -- This is required to prevent conflicts with dirbuf
-        vim.g.disable_rainbow_csv_autodetect = 1
-    end,
-}
--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/vim-checkbox.lua
+++
@@ -1,4 +0,0 @@
-return {
-    "jkramer/vim-checkbox",
-    ft = "markdown",
-}
