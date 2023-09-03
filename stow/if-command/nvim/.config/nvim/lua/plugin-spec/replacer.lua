0e13df6336e1c6502a31b0d459891f15cb8520ae
Andrew Ferrier <andrewferrier@example.com>
2023-09-03 16:58:41 +0100

    chore: Remove replacer.nvim; buggy

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/replacer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "gabrielpoca/replacer.nvim",
-    ft = "qf",
-    config = function()
-        vim.api.nvim_create_user_command("Replacer", function()
-            require("replacer").run()
-        end, { desc = "Use replacer.nvim for quickfix list" })
-    end,
-}
