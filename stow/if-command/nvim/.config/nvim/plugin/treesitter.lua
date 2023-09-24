5835e9bc65650cf845314b60826fc1b3c5515893
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-09-24 20:26:37 +0100

    chore: Remove dead param

--- stow/if-command/nvim/.config/nvim/plugin/treesitter.lua
+++ stow/if-command/nvim/.config/nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 if vim.fn.has("nvim-0.10.0") == 0 then
-    vim.api.nvim_create_user_command("InspectTree", function(opts)
+    vim.api.nvim_create_user_command("InspectTree", function()
         require("vim.treesitter.playground").inspect_tree()
     end, {})
 end
