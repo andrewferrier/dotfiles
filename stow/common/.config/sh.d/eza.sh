657fc3a8872d5d13458dc85baf30cbb445e6088d
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-10-03 22:03:36 +0100

    feat: Add '--extended' to eza

--- stow/common/.config/sh.d/eza.sh
+++ stow/common/.config/sh.d/eza.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if (command -v eza >/dev/null 2>&1); then
-    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --hyperlink --mounts"
+    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --hyperlink --mounts --extended"
 elif (command -v exa >/dev/null 2>&1); then
     alias ls="exa --time-style=long-iso --binary --group-directories-first --group --classify"
 fi
