da89d4953261e7cba3761404e7528c8570cea665
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-10-10 22:44:55 +0100

    feat: Add --color-scale to exa/eza

--- stow/common/.config/sh.d/eza.sh
+++ stow/common/.config/sh.d/eza.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if (command -v eza >/dev/null 2>&1); then
-    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --hyperlink --mounts --extended"
+    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --hyperlink --mounts --extended"
 elif (command -v exa >/dev/null 2>&1); then
-    alias ls="exa --time-style=long-iso --binary --group-directories-first --group --classify"
+    alias ls="exa --time-style=long-iso --binary --group-directories-first --group --classify --color-scale"
 fi
