5017e2bdf9f9a82620e3f7ddb3534c8d7ed4bee1
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-10-21 14:10:20 +0100

    perf: Remove --git-repos

--- stow/common/.config/sh.d/eza.sh
+++ stow/common/.config/sh.d/eza.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if (command -v eza >/dev/null 2>&1); then
-    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --git --git-repos --hyperlink --mounts --extended --no-quotes"
+    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --git --hyperlink --mounts --extended --no-quotes"
 elif (command -v exa >/dev/null 2>&1); then
     alias ls="exa --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --git"
 fi
