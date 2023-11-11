38674d61bec7ea0d8d0aa46f6606436d35bd3591
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-11-11 21:59:42 +0000

    feat: Implement todo-fzf

--- stow/common/.config/sh.d/todo.sh
+++ stow/common/.config/sh.d/todo.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 #
 # shellcheck disable=SC2086
-# shellcheck disable=SC2089
-# shellcheck disable=SC2090
 # shellcheck disable=SC2139
-# shellcheck disable=SC2312
 
 if (command -v todo >/dev/null 2>&1); then
     readonly FILTER_BOX="cut -d' ' -f3-"
     readonly PREVIEW="--preview 'todo show {1}'"
-    readonly OPTIONS="--ansi --tac --preview-window=top,8"
+    readonly OPTIONS="--ansi --tac --preview-window=top,5"
 
-    alias todo-delete="todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} --multi | cut -d' ' -f1 | xargs todo delete --yes"
-    alias todo-done="todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} --multi | cut -d' ' -f1 | xargs todo done"
+    readonly RELOAD="reload(todo --color always list | ${FILTER_BOX})"
 
-    alias todo-edit="todo edit \$(todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} | cut -d' ' -f1)"
-    alias todo-edit-raw="todo edit --raw \$(todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} | cut -d' ' -f1)"
+    readonly TODAY="\$(date +%Y-%m-%d)"
+    readonly TOMORROW="\$(date --date=tomorrow +%Y-%m-%d)"
 
-    alias todo-edit-today="todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} -m | cut -d' ' -f1 | xargs -n 1 todo edit --due \$(date +%Y-%m-%d)"
-    alias todo-edit-tomorrow="todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} -m | cut -d' ' -f1 | xargs -n 1 todo edit --due \$(date --date=tomorrow +%Y-%m-%d)"
+    readonly RELOAD_KEY="--bind \"ctrl-r:${RELOAD}\""
+    readonly DELETE_KEY="--bind \"ctrl-delete:execute(todo delete --yes {1})+${RELOAD}\""
+    readonly ENTER_KEY="--bind \"enter:execute(todo done {1})+${RELOAD}\""
+    readonly EDIT_KEY="--bind \"ctrl-e:execute(todo edit {1})+${RELOAD}\""
+    readonly EDIT_RAW_KEY="--bind \"ctrl-a:execute(todo edit --raw {1})+${RELOAD}\""
+    readonly EDIT_TODAY_KEY="--bind \"ctrl-y:execute(todo edit --due ${TODAY} {1})+${RELOAD}\""
+    readonly EDIT_TOMORROW_KEY="--bind \"ctrl-w:execute(todo edit --due ${TOMORROW} {1})+${RELOAD}\""
+    readonly NEW_KEY="--bind \"ctrl-n:execute(todo new --interactive)+${RELOAD}\""
 
-    alias todo-new-today="todo new --due today"
-    alias todo-new-tomorrow="todo new --due tomorrow"
+    readonly KEYS_1="${RELOAD_KEY} ${DELETE_KEY} ${ENTER_KEY}"
+    readonly KEYS_2="${EDIT_KEY} ${EDIT_RAW_KEY} ${EDIT_TODAY_KEY} ${EDIT_TOMORROW_KEY} ${NEW_KEY}"
+
+    alias todo-fzf="todo --color always list | ${FILTER_BOX} | fzf ${PREVIEW} ${OPTIONS} ${KEYS_1} ${KEYS_2}"
 fi
