9af06e3444d062bfaa2e05887b93306ca665268b
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-11-17 22:15:57 +0000

    fix: Use consistent keys for oil

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/oil.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/oil.lua
@@ -4,6 +4,10 @@ return {
         local opts = {
             skip_confirm_for_simple_edits = true,
             view_options = { show_hidden = true },
+            keymaps = {
+                ["<C-s>"] = "actions.select_split",
+                ["<C-v>"] = "actions.select_vsplit",
+            },
         }
 
         if not vim.fn.executable("trash-put") then
