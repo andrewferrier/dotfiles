531a8752c03164b664da8509129c8d7fb8cd40ba
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-12-17 21:15:19 +0000

    refactor: Use XDG_CONFIG_HOME

--- stow/common/.config/sh-profile.d/misc-env-vars.sh
+++ stow/common/.config/sh-profile.d/misc-env-vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export HIGHLIGHT_OPTIONS="--style=base16/gruvbox-light-hard"
-export RIPGREP_CONFIG_PATH=~/.config/ripgrep/rc.conf
+export RIPGREP_CONFIG_PATH=${XDG_CONFIG_HOME}/ripgrep/rc.conf
 export FZF_DEFAULT_OPTS='--exact'
