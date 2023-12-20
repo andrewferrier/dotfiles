39aec0c0c770ded1edff4b4b4dbb11b2286982d3
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-12-20 13:27:20 +0000

    chore: Remove InspectTree command
    Already part of NeoVim 0.9

--- stow/if-command/nvim/.config/nvim/plugin/treesitter.lua
+++
@@ -1,5 +0,0 @@
-if vim.fn.has("nvim-0.10.0") == 0 then
-    vim.api.nvim_create_user_command("InspectTree", function()
-        require("vim.treesitter.playground").inspect_tree()
-    end, {})
-end
