09db70f902dff053da6d9dbf82b843f413708b47
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-12-22 19:19:03 +0000

    fix: Simplify and fix visual_selection_range for NeoVim 0.9

--- stow/if-command/nvim/.config/nvim/lua/utils.lua
+++ stow/if-command/nvim/.config/nvim/lua/utils.lua
@@ -1,42 +1,19 @@
 local M = {}
 
-local function get_marked_region(mark1, mark2, options)
-    local bufnr = 0
-    local adjust = options.adjust
-        or function(pos1, pos2)
-            return pos1, pos2
-        end
-    local regtype = options.regtype or vim.fn.visualmode()
-    local selection = options.selection or (vim.o.selection ~= "exclusive")
-
-    local pos1 = vim.fn.getpos(mark1)
-    local pos2 = vim.fn.getpos(mark2)
-    pos1, pos2 = adjust(pos1, pos2)
-
-    local start = { pos1[2] - 1, pos1[3] - 1 + pos1[4] }
-    local finish = { pos2[2] - 1, pos2[3] - 1 + pos2[4] }
-
-    -- Return if start or finish are invalid
-    if start[2] < 0 or finish[1] < start[1] then
-        return
-    end
-
-    local region = vim.region(bufnr, start, finish, regtype, selection)
-    return region, start, finish
-end
-
 M.visual_selection_range = function()
     -- See https://github.com/neovim/neovim/pull/13896#issuecomment-1621702052
-    local r = vim.region(0, "'<", "'>", vim.fn.visualmode(), true)
-    local text = ""
-    local maxcol = vim.v.maxcol
-    for line, cols in vim.spairs(r) do
-        local endcol = cols[2] == maxcol and -1 or cols[2]
-        local chunk =
-            vim.api.nvim_buf_get_text(0, line, cols[1], line, endcol, {})[1]
-        text = ("%s%s\n"):format(text, chunk)
+    local s_start = vim.fn.getpos("'<")
+    local s_end = vim.fn.getpos("'>")
+    local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+    local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+    lines[1] = string.sub(lines[1], s_start[3], -1)
+    if n_lines == 1 then
+        lines[n_lines] =
+            string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+    else
+        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
     end
-    return text
+    return table.concat(lines, "\n")
 end
 
 M.job_stderr = function(_, stderr_line, _)
