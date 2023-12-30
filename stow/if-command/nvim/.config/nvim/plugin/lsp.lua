97e9a319b00bdad99101c47bd21ab0ca59f67935
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-12-30 00:25:57 +0000

    refactor: Remove fussy check for terraformls

--- stow/if-command/nvim/.config/nvim/plugin/lsp.lua
+++ stow/if-command/nvim/.config/nvim/plugin/lsp.lua
@@ -29,33 +29,8 @@ local function supports_method(method)
     return false
 end
 
-local function is_lsp_loaded(client_name)
-    for _, client in pairs(vim.lsp.buf_get_clients(0)) do
-        if client.name == client_name then
-            return true
-        end
-    end
-
-    return false
-end
-
-local function is_correct_lsp_loaded(filetype)
-    if filetype == "terraform" then
-        return is_lsp_loaded("terraformls")
-    else
-        return true
-    end
-end
-
 local function lsp_document_format()
-    if not is_correct_lsp_loaded(vim.bo.filetype) then
-        vim.notify(
-            "Required LSPs not yet loaded, please wait a bit and retry.",
-            vim.log.levels.ERROR
-        )
-
-        return
-    elseif
+    if
         vim.fn.has("nvim-0.10.0") == 0
         or supports_method(vim.lsp.protocol.Methods.textDocument_formatting)
     then
