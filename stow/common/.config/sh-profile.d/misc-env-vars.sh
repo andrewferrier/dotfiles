f5d5c9e8dae8f76925b38c93ed68c7e0fc03e438
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-01 15:29:41 +0000

    feat: Implement gruvbox colors for fzf

--- stow/common/.config/sh-profile.d/misc-env-vars.sh
+++ stow/common/.config/sh-profile.d/misc-env-vars.sh
@@ -2,4 +2,8 @@
 
 export HIGHLIGHT_OPTIONS="--style=base16/gruvbox-light-hard"
 export RIPGREP_CONFIG_PATH=${XDG_CONFIG_HOME}/ripgrep/rc.conf
-export FZF_DEFAULT_OPTS='--exact'
+export FZF_DEFAULT_OPTS_INITIAL='--exact'
+
+# This sets a default for when ~/.local/bin/common/day-night-terminal-setup is
+# not used for some reason
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS_INITIAL
