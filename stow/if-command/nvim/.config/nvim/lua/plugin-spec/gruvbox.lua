14efdff31d60c876958b9035333fb335a3625aa7
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-02 13:16:58 +0000

    fix: SignColumn color on NeoVim <0.10

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
@@ -13,23 +13,30 @@ return {
             vim.opt.background = "dark"
         end
 
+        local overrides = {
+            TermCursorNC = { bg = "#00DD00" },
+
+            -- See https://github.com/ellisonleao/gruvbox.nvim/issues/230#issuecomment-1493883602
+            GruvboxAquaSign = { bg = "" },
+            GruvboxBlueSign = { bg = "" },
+            GruvboxGreenSign = { bg = "" },
+            GruvboxOrangeSign = { bg = "" },
+            GruvboxPurpleSign = { bg = "" },
+            GruvboxRedSign = { bg = "" },
+            GruvboxYellowSign = { bg = "" },
+
+            ["@lsp.mod.readonly"] = { bold = true },
+        }
+
+        if vim.fn.has("nvim-0.10.0") == 0 then
+            -- This is a workaround; see ~/.config/nvim/plugin/status-column.lua
+            overrides["SignColumn"] = { link = "Normal" }
+        end
+
         require("gruvbox").setup({
             contrast = "hard",
             dim_inactive = true,
-            overrides = {
-                TermCursorNC = { bg = "#00DD00" },
-
-                -- See https://github.com/ellisonleao/gruvbox.nvim/issues/230#issuecomment-1493883602
-                GruvboxAquaSign = { bg = "" },
-                GruvboxBlueSign = { bg = "" },
-                GruvboxGreenSign = { bg = "" },
-                GruvboxOrangeSign = { bg = "" },
-                GruvboxPurpleSign = { bg = "" },
-                GruvboxRedSign = { bg = "" },
-                GruvboxYellowSign = { bg = "" },
-
-                ["@lsp.mod.readonly"] = { bold = true },
-            },
+            overrides = overrides,
             italic = {
                 strings = false,
                 operators = false,
