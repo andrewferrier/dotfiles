47497db6c4d59272f6ea50320db53f0ea59fd43f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-02 13:09:31 +0000

    fix: Handle missing DATA_SKELETON

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/esqueleto.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/esqueleto.lua
@@ -1,16 +1,20 @@
 return {
     "cvigilv/esqueleto.nvim",
     config = function()
-        require("esqueleto").setup({
-            directories = {
-                vim.fn.stdpath("config") .. "/skeleton",
-                vim.fn.stdpath("data") .. "/skeleton",
-            },
-            patterns = vim.list_extend(
-                vim.fn.readdir(vim.fn.stdpath("config") .. "/skeleton"),
-                vim.fn.readdir(vim.fn.stdpath("data") .. "/skeleton")
-            ),
-        })
+        local DATA_SKELETON = vim.fn.stdpath("data") .. "/skeleton"
+        local CONFIG_SKELETON = vim.fn.stdpath("config") .. "/skeleton"
+
+        if vim.fn.isdirectory(DATA_SKELETON) == 1 then
+            require("esqueleto").setup({
+                directories = { CONFIG_SKELETON, DATA_SKELETON },
+                patterns = vim.list_extend(CONFIG_SKELETON, DATA_SKELETON),
+            })
+        else
+            require("esqueleto").setup({
+                directories = { CONFIG_SKELETON },
+                patterns = { CONFIG_SKELETON },
+            })
+        end
     end,
     version = "*",
 }
