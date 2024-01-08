79bcbdd7984f27ceede450d287c8b4d0cf6cb56c
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-08 22:28:40 +0000

    fix: exa → eza

--- stow/common/.config/sh.d/deprecated-commands.sh
+++ stow/common/.config/sh.d/deprecated-commands.sh
@@ -4,5 +4,5 @@ alias id3v2='echo "Do not use id3v2: use eyeD3 instead, it'"'"'s more modern !"'
 alias xz="echo 'Do not use xz: https://www.nongnu.org/lzip/xz_inadequate.html !'"
 
 if (! command -v tree >/dev/null 2>&1); then
-    alias tree='echo "Use exa --tree instead!"'
+    alias tree='echo "Use eza --tree instead!"'
 fi
