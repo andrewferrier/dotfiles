73a74dd192f61289b9f2b8bbb5e5b01f1edae591
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-08 18:20:49 +0000

    feat: Add homebrew site-functions

--- stow/if-command/zsh/.config/zsh.d/75-fpath.zsh
+++ stow/if-command/zsh/.config/zsh.d/75-fpath.zsh
@@ -1,5 +1,10 @@
 typeset -U fpath
+
 fpath+=(/usr/local/share/zsh/functions)
 fpath+=(/usr/local/share/zsh/site-functions)
 fpath+=(/usr/share/zsh/site-functions)
 fpath+=(/usr/share/zsh/vendor-completions)
+
+if type brew &>/dev/null; then
+    fpath+=($(brew --prefix)/share/zsh/site-functions)
+fi
