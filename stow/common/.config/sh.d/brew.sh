95434939a626ad37b3bc2d6a7979256db323bab5
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-08 18:27:53 +0000

    fix: Brewfile path

--- stow/common/.config/sh.d/brew.sh
+++ stow/common/.config/sh.d/brew.sh
@@ -3,7 +3,7 @@
 if (command -v brew >/dev/null 2>&1); then
     alias brew-clean-casks="brew list --casks -1 | fzf -m --reverse | xargs -n1 brew uninstall"
     # shellcheck disable=SC2139
-    alias brew-clean-packages="comm -23 <(brew leaves | sort) <(cat ${HOME}/gitco/config/common/pkgs-macos/Brewfile | grep ^brew | cut -d'\"' -f2 | sort) | tac | fzf --multi --preview 'brew info {}' | xargs brew uninstall"
+    alias brew-clean-packages="comm -23 <(brew leaves | sort) <(cat ${HOME}/dotfiles/pkgs/macos/Brewfile | grep ^brew | cut -d'\"' -f2 | sort) | tac | fzf --multi --preview 'brew info {}' | xargs brew uninstall"
     alias brew-clean-taps="brew tap | fzf -m | xargs brew untap"
     alias brew-view-installed="(brew list --formula -1 && brew list --casks -1) | sort | fzf --preview 'brew info {}' --reverse"
 fi
