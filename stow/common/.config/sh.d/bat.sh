ebc9afee7d15d564776b85c000aba5bd5cea2df0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-17 19:46:36 +0000

    fix: MANPAGER to use --paging auto

--- stow/common/.config/sh.d/bat.sh
+++ stow/common/.config/sh.d/bat.sh
@@ -4,11 +4,11 @@ if (command -v batcat >/dev/null 2>&1); then
     alias bat=batcat
     alias cat=batcat
 
-    export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+    export MANPAGER="sh -c 'col -bx | batcat -l man -p --paging auto'"
     export MANROFFOPT="-c"
 elif (command -v bat >/dev/null 2>&1); then
     alias cat=bat
 
-    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    export MANPAGER="sh -c 'col -bx | bat -l man -p --paging auto'"
     export MANROFFOPT="-c"
 fi
