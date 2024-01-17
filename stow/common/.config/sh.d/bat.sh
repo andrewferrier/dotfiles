f39107de955064ee899843c106e6b95053c46824
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-17 21:20:07 +0000

    feat: Add less aliases for bat

--- stow/common/.config/sh.d/bat.sh
+++ stow/common/.config/sh.d/bat.sh
@@ -3,11 +3,13 @@
 if (command -v batcat >/dev/null 2>&1); then
     alias bat=batcat
     alias cat=batcat
+    alias less='bat --paging always'
 
     export MANPAGER="sh -c 'col --no-backspaces --spaces | batcat --style plain --language man --paging auto'"
     export MANROFFOPT="-c"
 elif (command -v bat >/dev/null 2>&1); then
     alias cat=bat
+    alias less='bat --paging always'
 
     export MANPAGER="sh -c 'col --no-backspaces --spaces | bat --style plain --language man --paging auto'"
     export MANROFFOPT="-c"
