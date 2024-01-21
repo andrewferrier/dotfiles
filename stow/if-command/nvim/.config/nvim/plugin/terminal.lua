f1a3497674666cd527d70be9c9a183888cb6c68a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-01-21 23:40:53 +0000

    refactor: Remove terminal Meta keys

--- stow/if-command/nvim/.config/nvim/plugin/terminal.lua
+++ stow/if-command/nvim/.config/nvim/plugin/terminal.lua
@@ -3,8 +3,8 @@ local id = vim.api.nvim_create_augroup("neovim_terminal", {})
 vim.api.nvim_create_autocmd("TermOpen", {
     group = id,
     callback = function()
-        require('filetype.status_column').remove_all()
-    end
+        require("filetype.status_column").remove_all()
+    end,
 })
 
 vim.api.nvim_create_autocmd("TermClose", {
@@ -14,11 +14,14 @@ vim.api.nvim_create_autocmd("TermClose", {
     end,
 })
 
-local OPTS = { silent = true, unique = true }
-
 -- Set window navigation to work like in other windows
 local map = function(lhs, rhs)
-    vim.keymap.set("t", lhs, "<C-\\><C-N>" .. rhs, OPTS)
+    vim.keymap.set(
+        "t",
+        lhs,
+        "<C-\\><C-N>" .. rhs,
+        { silent = true, unique = true }
+    )
 end
 
 map("<C-W>+", "<C-W>+")
@@ -37,8 +40,3 @@ map("<C-W>c", ":close<CR>")
 map("<C-W>n", ":new<CR>")
 map("<C-W>o", ":only<CR>")
 map("<C-W>w", "<C-W><C-W>")
-
--- For some reason these need explicit remapping
-vim.keymap.set("t", "<M-Left>", "<M-Left>", OPTS)
-vim.keymap.set("t", "<M-Right>", "<M-Right>", OPTS)
-vim.keymap.set("t", "<M-Up>", "<M-Up>", OPTS)
