066cce40f9c00cbe6732dfd439b1e55c488b9040
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-05 21:26:59 +0000

    perf: Spelling is not expensive

--- stow/if-command/nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ stow/if-command/nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,8 +1,4 @@
 require("filetype.text").setup()
 
-if require("utils").is_large_file() then
-    vim.opt_local.spell = false
-end
-
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
