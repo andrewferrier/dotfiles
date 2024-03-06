50ffb6445fc9dc4fe9273e4fd8d64b1ecd6b3083
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-06 19:09:03 +0000

    perf: Lazy-load copilot

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/copilot.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/copilot.lua
@@ -1,20 +1,25 @@
 return {
     "github/copilot.vim",
+    keys = {
+        {
+            "yop",
+            function()
+                if vim.b.copilot_enabled then
+                    vim.b.copilot_enabled = false
+                    vim.cmd.redrawstatus()
+                    vim.notify("Copilot disabled.")
+                else
+                    vim.b.copilot_enabled = true
+                    vim.cmd.redrawstatus()
+                    vim.notify("Copilot enabled.")
+                end
+            end,
+            mode = "n",
+        },
+    },
     config = function()
         vim.g.copilot_filetypes = {
             ["*"] = false,
         }
-
-        vim.keymap.set("n", "yop", function()
-            if vim.b.copilot_enabled then
-                vim.b.copilot_enabled = false
-                vim.cmd.redrawstatus()
-                vim.notify("Copilot disabled.")
-            else
-                vim.b.copilot_enabled = true
-                vim.cmd.redrawstatus()
-                vim.notify("Copilot enabled.")
-            end
-        end, { desc = "Toggle Copilot", unique = true })
     end,
 }
