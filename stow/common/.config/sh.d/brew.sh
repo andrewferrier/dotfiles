b8711342dbeca92619f447a59c9c46ac23d91d44
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-06 17:38:58 +0000

    feat: Preview casks

--- stow/common/.config/sh.d/brew.sh
+++ stow/common/.config/sh.d/brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if (command -v brew >/dev/null 2>&1); then
-    alias brew-clean-casks="brew list --casks -1 | fzf -m --reverse | xargs -n1 brew uninstall"
+    alias brew-clean-casks="brew list --casks -1 | fzf -m --reverse --preview 'brew info {}' | xargs -n1 brew uninstall"
     # shellcheck disable=SC2139
     alias brew-clean-packages="comm -23 <(brew leaves | sort) <(cat ${HOME}/dotfiles/pkgs/macos/Brewfile | grep ^brew | cut -d'\"' -f2 | sort) | tac | fzf --multi --preview 'brew info {}' | xargs brew uninstall"
     alias brew-clean-taps="brew tap | fzf -m | xargs brew untap"
