ecdc4806877b0b83abf7b1682a6cb6c52dffb86e
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-06 17:47:43 +0000

    fix: Handle breaking change in gx.nvim

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/gx.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/gx.lua
@@ -1,7 +1,8 @@
 return {
     "chrishrb/gx.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = "gx",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
     config = true,
     enabled = function()
         return vim.fn.has("nvim-0.10.0") == 0
