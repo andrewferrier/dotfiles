e451c5d165c4b5b5650873c635a7765185f267e0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-06 21:40:39 +0000

    perf: Improve startup by removing ensure_installed

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/nvim-treesitter.lua
@@ -1,21 +1,18 @@
-local configure = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
-
-        -- Ignore parsers I've had issues with and don't need
-        ignore_install = { "julia", "smali" },
-
-        highlight = { enable = true },
-        refactor = { smart_rename = { enable = true } },
-        indent = { enable = true },
-        endwise = { enable = true },
-    })
-end
-
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = configure,
+    build = function()
+        vim.cmd("TSInstall all")
+        vim.cmd("TSUpdate")
+    end,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- Don't use ensure_installed; slows down startup
+            highlight = { enable = true },
+            refactor = { smart_rename = { enable = true } },
+            indent = { enable = true },
+            endwise = { enable = true },
+        })
+    end,
     event = { "BufReadPre", "BufNewFile" },
     -- Re-enable once on NeoVim 0.10
     -- version = "*",
