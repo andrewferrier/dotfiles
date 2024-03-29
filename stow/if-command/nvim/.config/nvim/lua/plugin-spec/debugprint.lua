1cd36517d376601c2d7acb1560b438f4e3cb4499
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-03-29 17:04:28 +0000

    fix: Use mini.nvim dep for debugprint

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/debugprint.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/debugprint.lua
@@ -1,5 +1,8 @@
 return {
     url = "git@github.com:andrewferrier/debugprint.nvim",
+    dependencies = {
+        "echasnovski/mini.nvim"
+    },
     opts = {},
     -- Don't use 'keys'; won't work if visual mode is used first
 }
