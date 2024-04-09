0bb2a1af8a2671eb16b9075ae3aa39b80a6cb441
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-04-09 22:08:10 +0100

    feat: Use --info=inline-right where appropriate

--- stow/common/.config/sh-profile.d/misc-env-vars.sh
+++ stow/common/.config/sh-profile.d/misc-env-vars.sh
@@ -2,7 +2,14 @@
 
 export PAGER=less
 export RIPGREP_CONFIG_PATH=${XDG_CONFIG_HOME:-${HOME}/.config}/ripgrep/rc.conf
+
+# shellcheck disable=SC2312
+FZF_VERSION=$(fzf --version | cut -d'.' -f2)
+
 export FZF_DEFAULT_OPTS_INITIAL='--exact'
+if ((FZF_VERSION >= 42)); then
+    export FZF_DEFAULT_OPTS_INITIAL="$FZF_DEFAULT_OPTS_INITIAL --info=inline-right"
+fi
 
 # This sets a default for when ~/.local/bin/common/day-night-terminal-setup is
 # not used for some reason
