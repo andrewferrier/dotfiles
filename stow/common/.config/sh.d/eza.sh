d5cc82043ff46e518b25318e12f3c8f4271b991a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-05-12 15:24:14 +0100

    feat: Add icons for eza

--- stow/common/.config/sh.d/eza.sh
+++ stow/common/.config/sh.d/eza.sh
@@ -2,5 +2,5 @@
 
 if (command -v eza >/dev/null 2>&1); then
     export EZA_MIN_LUMINANCE=60
-    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --git --hyperlink --mounts --no-quotes"
+    alias ls="eza --time-style=long-iso --binary --group-directories-first --group --classify --color-scale --git --hyperlink --mounts --no-quotes --icons=auto"
 fi
