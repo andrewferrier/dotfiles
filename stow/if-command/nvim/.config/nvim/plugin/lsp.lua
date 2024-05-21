d824552e942762b4f08ae9433e6084583789392a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-05-21 22:04:05 +0100

    fix: Use is_enabled bufnr

--- stow/if-command/nvim/.config/nvim/plugin/lsp.lua
+++ stow/if-command/nvim/.config/nvim/plugin/lsp.lua
@@ -74,7 +74,7 @@ end
 local function keybindings_inlayhints(bufnr)
     vim.keymap.set("n", "yoi", function()
         vim.lsp.inlay_hint.enable(
-            not vim.lsp.inlay_hint.is_enabled(bufnr),
+            not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
             { bufnr = bufnr }
         )
     end, { buffer = bufnr, desc = "Toggle inlay hints" })
