827fa286c79446368197a2891ad1b033efe0f8d2
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-06-03 17:49:55 +0100

    fix: Don't switch GTK theme on Mac

--- stow/desktop/.local/share/dark-mode.d/gtk-theme.sh
+++ stow/desktop/.local/share/dark-mode.d/gtk-theme.sh
@@ -7,6 +7,8 @@ set -o pipefail
 
 shopt -s inherit_errexit
 
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
-echo 'Adwaita-dark' > "$HOME/.config/gtk-2.0/gtkrc2"
-printf "[Settings]\nAdwaita-dark\n" > "$HOME/.config/gtk-3.0/settings.ini"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+    echo 'Adwaita-dark' >"$HOME/.config/gtk-2.0/gtkrc2"
+    printf "[Settings]\nAdwaita-dark\n" >"$HOME/.config/gtk-3.0/settings.ini"
+fi
--- stow/desktop/.local/share/light-mode.d/gtk-theme.sh
+++ stow/desktop/.local/share/light-mode.d/gtk-theme.sh
@@ -7,6 +7,8 @@ set -o pipefail
 
 shopt -s inherit_errexit
 
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita
-echo 'Adwaita' > "$HOME/.config/gtk-2.0/gtkrc2"
-printf "[Settings]\nAdwaita\n" > "$HOME/.config/gtk-3.0/settings.ini"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+    echo 'Adwaita' >"$HOME/.config/gtk-2.0/gtkrc2"
+    printf "[Settings]\nAdwaita\n" >"$HOME/.config/gtk-3.0/settings.ini"
+fi
