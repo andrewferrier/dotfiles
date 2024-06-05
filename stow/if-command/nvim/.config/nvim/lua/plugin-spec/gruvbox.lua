13edcfb22b01e7209a98f205b3674f0afee418ef
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-06-05 18:29:16 +0100

    Revert "fix: Use built-in NeoVim background detection"
    This reverts commit 7d0a3974a8a9729f50d7d3d3dae146b060dd8f96.

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
@@ -1,6 +1,18 @@
 return {
     "ellisonleao/gruvbox.nvim",
     config = function()
+        local STATEFILE = vim.fn.expand("~/.cache/day-night/state-terminal")
+
+        ---@cast STATEFILE string
+        if
+            vim.fn.filereadable(STATEFILE)
+            and vim.fn.index(vim.fn.readfile(STATEFILE), "day") >= 0
+        then
+            vim.opt.background = "light"
+        else
+            vim.opt.background = "dark"
+        end
+
         local overrides = {
             NormalFloat = { link = "Normal" },
             TermCursorNC = { bg = "#00DD00" },
