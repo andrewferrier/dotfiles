90ee9522f01bfa9af94874cfd4ed37e35fc0931f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-06-13 00:30:59 +0100

    refactor: Consolidate FZF colors

--- stow/desktop/.local/share/dark-mode.d/kitty-config.sh
+++ stow/desktop/.local/share/dark-mode.d/kitty-config.sh
@@ -10,12 +10,12 @@ shopt -s inherit_errexit
 source "${HOME}/.config/sh-profile.d/misc-env-vars.sh"
 
 MODE=${1:-dark}
-FZF_COLORS=${2:-$FZF_DARK_COLORS}
+FZF_COLORS=${2:-$FZF_DEFAULT_OPTS_DARK_COLORS}
 
 KITTY_TRANSIENT="${HOME}/.config/kitty/transient.conf"
 
 cat << EOF > "$KITTY_TRANSIENT"
 include ~/.config/kitty/colors-${MODE}.conf
-env FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS_INITIAL $FZF_COLORS
+env FZF_DEFAULT_OPTS=${FZF_COLORS}
 env BAT_THEME=gruvbox-${MODE}
 EOF
--- stow/desktop/.local/share/light-mode.d/kitty-config.sh
+++ stow/desktop/.local/share/light-mode.d/kitty-config.sh
@@ -10,4 +10,4 @@ shopt -s inherit_errexit
 source "${HOME}/.config/sh-profile.d/misc-env-vars.sh"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-"${SCRIPT_DIR}/../dark-mode.d/kitty-config.sh" light "$FZF_LIGHT_COLORS"
+"${SCRIPT_DIR}/../dark-mode.d/kitty-config.sh" light "$FZF_DEFAULT_OPTS_LIGHT_COLORS"
