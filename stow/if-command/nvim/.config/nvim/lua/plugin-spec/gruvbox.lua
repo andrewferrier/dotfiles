61179b1be1cfeeaf4dbbd0aeefe34f14d72dd20d
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-06-21 00:55:44 +0100

    fix: Correct statuscolumn on NeoVim nightly

--- stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-spec/gruvbox.lua
@@ -14,6 +14,7 @@ return {
         end
 
         local overrides = {
+            SignColumn = { link = "LineNr" },
             TermCursorNC = { bg = "#00DD00" },
             ["@lsp.mod.readonly"] = { bold = true },
         }
--- stow/if-command/nvim/.config/nvim/plugin/status-column.lua
+++ stow/if-command/nvim/.config/nvim/plugin/status-column.lua
@@ -1,31 +1,57 @@
-_G.custom_statuscol = function()
-    if vim.v.virtnum == 0 then
-        local signs = vim.fn.sign_getplaced(
-            vim.fn.bufname(),
-            { group = "*", lnum = tostring(vim.v.lnum) }
-        )[1].signs[1]
-
-        if signs ~= nil then
-            if vim.o.relativenumber == true then
-                return "%s%=%r "
-            else
-                return "%s%=%l "
-            end
-        else
-            if vim.o.relativenumber == true then
-                return "%=%r "
-            else
-                return "%=%l "
-            end
-        end
-    else
-        return ""
-    end
+---@param linenum integer
+---@return boolean
+local signs_on_line = function(linenum)
+    return vim.fn.sign_getplaced(
+        vim.fn.bufname(),
+        { group = "*", lnum = tostring(linenum) }
+    )[1].signs[1] ~= nil
 end
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+if vim.fn.has("nvim-0.11.0") == 1 then
+    ---@return string
+    _G.custom_statuscol = function()
+        if vim.v.virtnum ~= 0 then
+            return ""
+        end
 
+        if vim.o.number == false then
+            if vim.o.relativenumber == true then
+                return "%s%l "
+            else
+                return "%s" .. vim.v.lnum .. " "
+            end
+        else
+            return "%s"
+        end
+    end
+
+    vim.opt.number = false
+else
+    ---@return string
+    _G.custom_statuscol = function()
+        if vim.v.virtnum ~= 0 then
+            return ""
+        end
+
+        local line
+
+        if signs_on_line(vim.v.lnum) then
+            line = "%s"
+        else
+            line = ""
+        end
+
+        if vim.o.relativenumber == true then
+            return line .. "%=%r "
+        else
+            return line .. "%=%l "
+        end
+    end
+
+    vim.opt.number = true
+end
+
+vim.opt.relativenumber = true
 vim.opt.numberwidth = 3
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes:1"
 vim.opt.statuscolumn = "%{%v:lua.custom_statuscol()%}"
