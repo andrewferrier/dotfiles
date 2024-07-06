b9d3ecfe1c4642e9e6cf66a7f8390809a00e3d5c
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-06 16:25:23 +0100

    feat: Use my own package spec for debugprint

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/debugprint.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/debugprint.lua
@@ -1,13 +1,3 @@
 return {
-    url = "git@github.com:andrewferrier/debugprint.nvim",
-    opts = {},
-    keys = {
-        { "g?", mode = "n" },
-        { "g?", mode = "x" },
-    },
-    cmd = {
-        "ToggleCommentDebugPrints",
-        "DeleteDebugPrints",
-    },
-    lazy = true,
+    "git@github.com:andrewferrier/debugprint.nvim",
 }
