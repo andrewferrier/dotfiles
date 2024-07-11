04ef95fe1909f35c43067ec0e0cd4d89383fda03
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-11 22:20:26 +0100

    fix: Reinstate debugprint.lua config

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/debugprint.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/debugprint.lua
@@ -1,3 +1,12 @@
 return {
     "git@github.com:andrewferrier/debugprint.nvim",
+    opts = {},
+    keys = {
+        { "g?", mode = "n" },
+        { "g?", mode = "x" },
+    },
+    cmd = {
+        "ToggleCommentDebugPrints",
+        "DeleteDebugPrints",
+    },
 }
