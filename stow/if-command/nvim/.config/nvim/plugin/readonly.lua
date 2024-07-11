b173c1a818550573b73f99cfed3b964a022960c5
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-11 22:39:34 +0100

    refactor: Inline readonly.lua

--- stow/if-command/nvim/.config/nvim/plugin/readonly.lua
+++ stow/if-command/nvim/.config/nvim/plugin/readonly.lua
@@ -1,4 +1,18 @@
 vim.keymap.set("n", "cvr", function()
-    require("readonly").make_readonly()
+    vim.bo.readonly = true
+    vim.opt_local.concealcursor = "nc"
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.list = false
+    vim.opt_local.spell = false
+    vim.opt_local.signcolumn = "no"
+
+    require("gitsigns").detach()
+
+    vim.diagnostic.enable(false, { bufnr = 0 })
+
+    if vim.b.text_based_filetype then
+        require("wrapping").soft_wrap_mode()
+    end
+
     vim.notify("Effective R/O on.")
 end, { desc = "Turn on effective r/o", unique = true })
