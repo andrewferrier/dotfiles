3a0181f098f313f5585d77644b2e98dba24802a1
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-13 17:16:14 +0100

    feat: Add hardtime.nvim to see if it's useful

---
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/hardtime.lua
@@ -0,0 +1,12 @@
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+        disabled_keys = {
+            ["<Up>"] = {},
+            ["<Down>"] = {},
+            ["<Left>"] = {},
+            ["<Right>"] = {},
+        },
+    },
+}
