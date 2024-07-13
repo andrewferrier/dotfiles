a8c6380b0bbbe383438fcc8b8887ffd1ffbd14ee
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-13 18:06:03 +0100

    fix: Change dark mode immediately for GTK

--- stow/desktop/.local/share/dark-mode.d/gtk-theme.sh
+++ stow/desktop/.local/share/dark-mode.d/gtk-theme.sh
@@ -9,6 +9,11 @@ THEME_NAME=${1:-Adwaita-dark}
 DARK_INT=${2:-1}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    mkdir -p \
+        "$HOME/.config/gtk-2.0" \
+        "$HOME/.config/gtk-3.0"
+
     echo "gtk-theme-name=\"${THEME_NAME}\"" >"$HOME/.config/gtk-2.0/gtkrc2"
     printf "[Settings]\ngtk-theme-name=%s\ngtk-application-prefer-dark-theme=%s\n" "${THEME_NAME}" "${DARK_INT}" >"$HOME/.config/gtk-3.0/settings.ini"
+    xfconf-query -c xsettings -p /Net/ThemeName -s "${THEME_NAME}"
 fi
