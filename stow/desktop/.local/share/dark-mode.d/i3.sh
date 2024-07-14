da7ed9dec450d529f19a435720653db402ccee42
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-14 23:20:14 +0100

    fix: Only change i3 configuration on Linux

--- stow/desktop/.local/share/dark-mode.d/i3.sh
+++ stow/desktop/.local/share/dark-mode.d/i3.sh
@@ -6,5 +6,7 @@ set -o pipefail
 
 MODE_NAME=${MODE_NAME:-"dark"}
 
-cp "${HOME}/.config/i3/i3-${MODE_NAME}-colors.conf" "${HOME}/.cache/day-night/i3-colors.conf"
-i3-msg -s /run/user/1000/i3/ipc-socket.* reload
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cp "${HOME}/.config/i3/i3-${MODE_NAME}-colors.conf" "${HOME}/.cache/day-night/i3-colors.conf"
+    i3-msg -s /run/user/1000/i3/ipc-socket.* reload
+fi
