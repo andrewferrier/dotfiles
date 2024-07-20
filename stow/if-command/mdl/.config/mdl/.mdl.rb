8c3d9dccd44a4cee20506af19c78d1a15fc530b6
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-20 14:21:35 +0100

    chore: Sort list

--- stow/if-command/mdl/.config/mdl/.mdl.rb
+++ stow/if-command/mdl/.config/mdl/.mdl.rb
@@ -8,6 +8,6 @@ exclude_rule 'MD009' # use null_ls instead, it's more accurate
 exclude_rule 'MD013'
 exclude_rule 'MD024' # it is valid to have multiple headers with the same content in multiple sections!
 exclude_rule 'MD025' # in documents with frontmatter, multiple top-level headers are fine
-exclude_rule 'MD041' # it is not necessarily true that the first line should be top-level, e.g. frontmatter
 exclude_rule 'MD030' # not currently possible to ask markdownlint to check for consistent lists
 exclude_rule 'MD033'
+exclude_rule 'MD041' # it is not necessarily true that the first line should be top-level, e.g. frontmatter
