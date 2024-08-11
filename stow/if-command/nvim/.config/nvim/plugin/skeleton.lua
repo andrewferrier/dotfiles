c36eaf7c8cef11693690fc722328729fd5174136
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-08-11 21:48:16 +0100

    Revert "refactor: skeleton → esqueleto.lua"
    This reverts commit 452a6caeba8b6de4985f2c70106ce27952ab921f.
    
    esqueleto is too complex and too buggy; it causes issues - for example
    commenting in lua files.

---
+++ stow/if-command/nvim/.config/nvim/plugin/skeleton.lua
@@ -0,0 +1,45 @@
+---@param tail string
+---@return string|nil
+local function get_skeleton_match(tail)
+    local skeleton =
+        vim.api.nvim_get_runtime_file("skeleton/" .. tail, false)[1]
+
+    if skeleton == "" then
+        return nil
+    else
+        return skeleton
+    end
+end
+
+---@return string|nil
+local function find_skeleton()
+    local skeleton = get_skeleton_match(vim.fn.expand("%:t"))
+
+    if skeleton == nil then
+        skeleton = get_skeleton_match(vim.fn.expand("%:e"))
+    end
+
+    local filetype = vim.o.filetype
+
+    if skeleton == nil and filetype ~= "" then
+        skeleton = get_skeleton_match(filetype)
+    end
+
+    return skeleton
+end
+
+local function check_for_skeleton()
+    local skeleton = find_skeleton()
+
+    if skeleton ~= nil then
+        vim.cmd.read(skeleton)
+        vim.cmd.normal({ args = { "kddG" }, bang = true })
+        vim.notify('Added skeleton ' .. skeleton)
+    end
+end
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+    group = vim.api.nvim_create_augroup("skeleton", {}),
+    pattern = "*",
+    callback = check_for_skeleton,
+})
