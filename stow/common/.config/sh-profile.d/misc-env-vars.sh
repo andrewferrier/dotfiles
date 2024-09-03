ad09f462ec4c37363ee5c61f8aa608493fb3b1bc
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-09-03 22:44:21 +0100

    fix: shellcheck error

--- stow/common/.config/sh-profile.d/misc-env-vars.sh
+++ stow/common/.config/sh-profile.d/misc-env-vars.sh
@@ -3,11 +3,10 @@
 export PAGER=less
 export RIPGREP_CONFIG_PATH=${XDG_CONFIG_HOME:-${HOME}/.config}/ripgrep/rc.conf
 
-# shellcheck disable=SC2312
-
 export FZF_DEFAULT_OPTS_INITIAL='--exact'
 
 if command -v fzf >/dev/null 2>/dev/null; then
+    # shellcheck disable=SC2312
     FZF_VERSION=$(fzf --version | cut -d'.' -f2)
     if ((FZF_VERSION >= 42)); then
         export FZF_DEFAULT_OPTS_INITIAL="$FZF_DEFAULT_OPTS_INITIAL --info=inline-right --no-separator"
