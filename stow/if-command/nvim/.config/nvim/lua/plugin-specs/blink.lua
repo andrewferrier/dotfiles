99cb3f5a917c88e899035f7683096ace7bf392e3
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-10-30 22:16:36 +0000

    feat: Experiment with blink

---
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -0,0 +1,9 @@
+return {
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "*",
+    opts = {
+        accept = { auto_brackets = { enabled = true } },
+        trigger = { signature_help = { enabled = true } },
+    },
+}
