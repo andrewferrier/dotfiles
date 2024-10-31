87a2ef8c11ec2018c675d09f1ff9bc6f1b19d301
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-10-31 10:25:13 +0000

    feat: Add <CR> as a keymapping

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -14,5 +14,21 @@ return {
                 force_version = "v0.5.0",
             },
         },
+        keymap = {
+            ["<CR>"] = { "select_and_accept", "fallback" },
+
+            -- The rest of this is the default keymapping
+            ["<C-e>"] = { "hide" },
+            ["<C-y>"] = { "select_and_accept" },
+
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+
+            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+            ["<Tab>"] = { "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        },
     },
 }
