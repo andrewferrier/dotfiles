f7b9c137c48329d9ba6bfe8f95a97855d2ece288
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-10-31 09:00:23 +0000

    fix: Work around issue with lsp method

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -1,9 +1,18 @@
 return {
     "saghen/blink.cmp",
     lazy = false,
-    version = "*",
+    -- FIXME: For now we are using the head version until
+    -- https://github.com/Saghen/blink.cmp/commit/193423ca584e4e1a9639d6c480a6b952db566c21
+    -- is merged into a stable version
+    --
+    -- version = "*"
     opts = {
         accept = { auto_brackets = { enabled = true } },
         trigger = { signature_help = { enabled = true } },
+        fuzzy = { -- FIXME: For now we are using the head version
+            prebuilt_binaries = {
+                force_version = "v0.5.0",
+            },
+        },
     },
 }
