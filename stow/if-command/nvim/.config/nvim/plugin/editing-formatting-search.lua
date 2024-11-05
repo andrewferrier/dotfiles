8feff74267a1094ea7b7505a57a7d8c07a001606
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-05 13:36:06 +0000

    chore: Remove code that's dead in NeoVim nightly

--- stow/if-command/nvim/.config/nvim/plugin/editing-formatting-search.lua
+++ stow/if-command/nvim/.config/nvim/plugin/editing-formatting-search.lua
@@ -20,16 +20,19 @@ vim.opt.diffopt:append("vertical")
 vim.cmd.iabbrev("zDATE", '<C-R>=strftime("%F")<CR>')
 vim.cmd.iabbrev("zDATETIME", '<C-R>=strftime("%FT%H:%M:%S")<CR>')
 
-vim.keymap.set(
-    "n",
-    "[<Space>",
-    "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>",
-    { desc = "Put empty line(s) above", unique = true }
-)
+if vim.fn.has("nvim-0.11.0") == 0 then
+    -- These are included in nightly: https://github.com/neovim/neovim/pull/30984
+    vim.keymap.set(
+        "n",
+        "[<Space>",
+        "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>",
+        { desc = "Put empty line(s) above", unique = true }
+    )
 
-vim.keymap.set(
-    "n",
-    "]<Space>",
-    "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>",
-    { desc = "Put empty line(s) below", unique = true }
-)
+    vim.keymap.set(
+        "n",
+        "]<Space>",
+        "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>",
+        { desc = "Put empty line(s) below", unique = true }
+    )
+end
