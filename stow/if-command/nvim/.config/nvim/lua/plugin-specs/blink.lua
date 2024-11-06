3a897a236d94e0d4b52a2fbd0615e7b628977f6f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-06 22:58:15 +0000

    fix: Simplify keymap

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -6,16 +6,9 @@ return {
         accept = { auto_brackets = { enabled = true } },
         trigger = { signature_help = { enabled = true } },
         keymap = {
-            ["<CR>"] = { "select_and_accept", "fallback" },
-            ["<Tab>"] = { "select_and_accept", "fallback" },
-
-            ["<C-e>"] = { "hide" },
-
-            ["<C-p>"] = { "select_prev", "fallback" },
-            ["<C-n>"] = { "select_next", "fallback" },
-
-            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+            preset = "default",
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
         },
     },
 }
