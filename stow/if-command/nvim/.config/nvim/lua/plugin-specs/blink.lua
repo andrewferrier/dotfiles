4840cbe9aa1d75297164cd6286f55f501c6a66da
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-06 22:58:01 +0000

    fix: Fix merged into stable blink

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -1,19 +1,10 @@
 return {
     "saghen/blink.cmp",
     lazy = false,
-    -- FIXME: For now we are using the head version until
-    -- https://github.com/Saghen/blink.cmp/commit/193423ca584e4e1a9639d6c480a6b952db566c21
-    -- is merged into a stable version
-    --
-    -- version = "*"
+    version = "*",
     opts = {
         accept = { auto_brackets = { enabled = true } },
         trigger = { signature_help = { enabled = true } },
-        fuzzy = { -- FIXME: For now we are using the head version
-            prebuilt_binaries = {
-                force_version = "v0.5.0",
-            },
-        },
         keymap = {
             ["<CR>"] = { "select_and_accept", "fallback" },
             ["<Tab>"] = { "select_and_accept", "fallback" },
