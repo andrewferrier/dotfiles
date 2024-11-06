4bc220feb35ec11cd57e1e7bfc5a6c0aaba5730c
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-06 17:48:39 +0000

    refactor: Move lazy configuration to separate file

---
+++ stow/if-command/nvim/.config/nvim/lua/lazy-configure.lua
@@ -0,0 +1,57 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+    vim.notify("Installing lazy...")
+
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
+
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup("plugin-specs", {
+    lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json",
+    install = {
+        colorscheme = { "gruvbox" },
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "2html",
+                "gzip",
+                "remote_plugins",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+
+                -- These lines are needed to avoid opening directories from command line in
+                -- netrw: https://github.com/elihunter173/dirbuf.nvim#notes
+                "netrw",
+                "netrwPlugin",
+            },
+            paths = { vim.fn.stdpath("data") .. "/skeleton" },
+        },
+    },
+    change_detection = {
+        enabled = false,
+    },
+})
