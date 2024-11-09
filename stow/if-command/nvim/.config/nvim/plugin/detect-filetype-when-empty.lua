4059916fec2238973d45246ea0fec55fb74082c2
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-09 15:40:29 +0000

    feat: Detect when filetype empty (shebang)

---
+++ stow/if-command/nvim/.config/nvim/plugin/detect-filetype-when-empty.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*",
+    callback = function()
+        if vim.bo.filetype == "" then
+            vim.cmd("filetype detect")
+        end
+    end,
+})
