0e79fe9ccf26ab50a6f142267506babfeab6d7e1
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-11-17 20:18:10 +0000

    chore: Remove snacks which are no longer enabled by default

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/snacks.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/snacks.lua
@@ -3,10 +3,6 @@ return {
     lazy = false,
     opts = {
         bigfile = { enabled = true },
-        notifier = { enabled = false },
-        quickfile = { enabled = false },
-        statuscolumn = { enabled = false },
-        words = { enabled = false },
     },
     keys = {
         {
