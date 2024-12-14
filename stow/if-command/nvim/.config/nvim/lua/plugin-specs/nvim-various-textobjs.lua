817fca5de9ea45eb94aecdc87cd6f42df0f9d2de
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-12-14 13:14:41 +0000

    refactor: inline keys for various-textobjs

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-various-textobjs.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-various-textobjs.lua
@@ -1,30 +1,30 @@
+local map = function(lhs, rhs, description)
+    return {
+        lhs,
+        '<cmd>lua require("various-textobjs").' .. rhs .. "<CR>",
+        mode = { "o", "x" },
+        desc = description,
+        unique = true,
+    }
+end
+
 return {
     "chrisgrieser/nvim-various-textobjs",
-    config = function()
-        local map = function(lhs, rhs, description)
-            vim.keymap.set(
-                { "o", "x" },
-                lhs,
-                '<cmd>lua require("various-textobjs").' .. rhs .. "<CR>",
-                { desc = description, unique = true }
-            )
-        end
-
+    keys = {
         map(
             "ii",
             'indentation("inner", "inner", "noBlanks")',
             "indentation not inc. blank lines"
-        )
+        ),
         map(
             "ai",
             'indentation("inner", "inner")',
             "indentation inc. blank lines"
-        )
-        map("ik", 'key("inner")', "'key'/lhs")
-        map("ak", 'key("outer")', "'key'/lhs inc. assignment")
-        map("iv", 'value("inner")', "'value'/rhs")
-        map("av", 'value("outer")', "'value'/rhs inc. terminator")
-        map("id", "diagnostic()", "diagnostic")
-    end,
-    event = "BufEnter",
+        ),
+        map("ik", 'key("inner")', "'key'/lhs"),
+        map("ak", 'key("outer")', "'key'/lhs inc. assignment"),
+        map("iv", 'value("inner")', "'value'/rhs"),
+        map("av", 'value("outer")', "'value'/rhs inc. terminator"),
+        map("id", "diagnostic()", "diagnostic"),
+    },
 }
