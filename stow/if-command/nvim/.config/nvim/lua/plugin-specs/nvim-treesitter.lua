847a9f6b77e14abe9db59558838fc4223fc094e3
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-01-04 23:59:37 +0000

    refactor: Simplify nvim-treesitter config

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
@@ -1,26 +1,24 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    main = "nvim-treesitter.configs",
     build = function()
         vim.cmd("TSInstall all")
         vim.cmd("TSUpdate")
     end,
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
-            sync_install = true,
-            highlight = { enable = true },
-            refactor = {
-                smart_rename = {
-                    enable = true,
-                    keymaps = {
-                        smart_rename = false,
-                    },
+    opts = {
+        sync_install = true,
+        highlight = { enable = true },
+        refactor = {
+            smart_rename = {
+                enable = true,
+                keymaps = {
+                    smart_rename = false,
                 },
             },
-            indent = { enable = true },
-            endwise = { enable = true },
-        })
-    end,
+        },
+        indent = { enable = true },
+        endwise = { enable = true },
+    },
 
     -- Don't use lazy initialization as TSInstall command is needed for build()
     -- function
