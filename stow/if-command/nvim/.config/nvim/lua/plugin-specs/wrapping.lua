f33dc994967c9e7b22a3a23392bfcd3a2d006126
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-01-05 00:10:42 +0000

    refactor: Simplify wrapping configuration

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/wrapping.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/wrapping.lua
@@ -1,18 +1,21 @@
 return {
     url = "git@github.com:andrewferrier/wrapping.nvim",
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("wrapping").setup({
-            create_keymaps = false,
-            ---@diagnostic disable-next-line: missing-fields
-            softener = { mail = true },
-            notify_on_switch = false,
-        })
-
-        vim.keymap.set("n", "yow", function()
-            require("wrapping").toggle_wrap_mode()
-        end, { desc = "Toggle wrap mode", unique = true })
-    end,
+    opts = {
+        create_keymaps = false,
+        softener = { mail = true },
+        notify_on_switch = false,
+    },
+    keys = {
+        {
+            "yow",
+            function()
+                require("wrapping").toggle_wrap_mode()
+            end,
+            desc = "Toggle wrap mode",
+            unique = true,
+        },
+    },
+    lazy = false,
     -- Set up mapping before mini.basics
     priority = 49,
 }
