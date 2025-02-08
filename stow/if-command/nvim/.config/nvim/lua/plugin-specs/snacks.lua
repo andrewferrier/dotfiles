8a22011fa780ced2128e8bbaae7a7e2fd947f40f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-02-08 22:45:22 +0000

    fix: Workaround for commit message bigfile issue

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/snacks.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/snacks.lua
@@ -3,9 +3,29 @@ return {
     "folke/snacks.nvim",
     version = "*",
     lazy = false,
-    opts = {
-        bigfile = { enabled = true },
-    },
+    config = function()
+        -- See https://github.com/folke/snacks.nvim/issues/1022
+        require("snacks").setup({
+            bigfile = { enabled = true },
+        })
+
+        local bigfile_callback = {
+            function(path, buf)
+                return vim.bo[buf]
+                        and vim.bo[buf].filetype ~= "bigfile"
+                        and path
+                        and vim.fn.getfsize(path) > 1.5 * 1024 * 1024
+                        and "bigfile"
+                    or nil
+            end,
+        }
+
+        vim.filetype.add({
+            filename = {
+                COMMIT_EDITMSG = bigfile_callback,
+            },
+        })
+    end,
     keys = {
         {
             "<Leader>r",
