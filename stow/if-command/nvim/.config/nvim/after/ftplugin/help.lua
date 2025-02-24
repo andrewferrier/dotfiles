e6ad2c3df9e74b110d9f0ed66725c560455d9d4c
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-02-24 22:07:40 +0000

    feat: Hide g== run messages

--- stow/if-command/nvim/.config/nvim/after/ftplugin/help.lua
+++ stow/if-command/nvim/.config/nvim/after/ftplugin/help.lua
@@ -2,3 +2,11 @@ vim.opt_local.wrap = true
 vim.opt_local.signcolumn = "no"
 
 require("utils").buf_set_q_key_to_quit()
+
+-- See https://www.reddit.com/r/neovim/comments/1isd2wl/comment/mdfr7gi/
+vim.api.nvim_buf_clear_namespace(
+    0,
+    vim.api.nvim_create_namespace("nvim.vimdoc.run_message"),
+    0,
+    -1
+)
