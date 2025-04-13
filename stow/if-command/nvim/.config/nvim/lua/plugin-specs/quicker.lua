45ee226fb807ff1fd5dad1e478b45a75e0cf628b
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-04-13 14:25:23 +0100

    feat: Add 'quicker' plugin

---
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/quicker.lua
@@ -0,0 +1,9 @@
+-- selene: allow(mixed_table)
+---@type LazyPluginSpec
+return {
+    "stevearc/quicker.nvim",
+    ft = "qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+}
