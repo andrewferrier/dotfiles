d1d58d5c8316592c1fb480594d8e9c4443602bdd
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-04-26 09:21:38 +0100

    feat: Support documentColor LSP method

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/mini/hipatterns.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/mini/hipatterns.lua
@@ -32,6 +32,9 @@ hipatterns.setup({
             pattern = if_textfile("%f[%w]()TODO()%f[%W]"),
             group = "MiniHipatternsTodo",
         },
+        -- hex_color is a fallback for filetypes not supported by an LSP which
+        -- can handle textDocument/documentColor (see
+        -- ~/.config/nvim/plugin/lsp.lua)
         hex_color = hipatterns.gen_highlighter.hex_color(),
     },
 })
--- stow/if-command/nvim/.config/nvim/plugin/lsp.lua
+++ stow/if-command/nvim/.config/nvim/plugin/lsp.lua
@@ -21,6 +21,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
             require("fzf-lua").lsp_code_actions,
             { buffer = bufnr, desc = "Apply code action" }
         )
+
+        if vim.fn.has("nvim-0.12.0") == 1 then
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+            if
+                client ~= nil
+                and client:supports_method("textDocument/documentColor")
+            then
+                vim.lsp.document_color.enable(true, bufnr)
+            end
+        end
     end,
 })
 
