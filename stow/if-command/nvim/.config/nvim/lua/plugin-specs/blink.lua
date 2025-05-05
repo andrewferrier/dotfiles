d7578527ce41912bfde98eaf2019887e587da293
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-05-05 19:32:20 +0100

    feat: Try super-tab mode instead

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/blink.lua
@@ -6,11 +6,7 @@ return {
     ---@type blink.cmp.Config
     opts = {
         keymap = {
-            preset = "default",
-            ["<CR>"] = { "select_and_accept", "fallback" },
-            ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-            ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
-            ["<C-Y>"] = { "fallback" },
+            preset = "super-tab",
         },
         sources = {
             default = { "lsp", "path", "buffer" },
