6f30a83b5bb75ea69bb497a5358f7e7b10c15ada
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-05-05 17:39:04 +0100

    feat: Add experimental support for extui

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/capture.lua
+++
@@ -1,8 +0,0 @@
----@type LazyPluginSpec
-return {
-    -- Eventually this can be replaced with
-    -- https://github.com/neovim/neovim/issues/5054
-    -- selene: allow(mixed_table)
-    "tyru/capture.vim",
-    cmd = "Capture",
-}
---
+++ stow/if-command/nvim/.config/nvim/plugin/cmdline-extui.lua
@@ -0,0 +1,12 @@
+if vim.fn.has("nvim-0.12.0") == 1 then
+    vim.o.cmdheight = 0
+
+    require("vim._extui").enable({
+        msg = {
+            pos = "box",
+            box = {
+                timeout = 3000,
+            },
+        },
+    })
+end
