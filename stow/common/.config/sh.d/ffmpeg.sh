2921da67b835cad365f630f02161afc3c322fcbc
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-05-07 09:19:51 +0100

    feat: Add ffmpeg-to-mp4-container function

--- stow/common/.config/sh.d/ffmpeg.sh
+++ stow/common/.config/sh.d/ffmpeg.sh
@@ -14,4 +14,11 @@ if (command -v ffmpeg >/dev/null 2>&1); then
 
         ffmpeg -i "${INPUT}" -filter:v scale="trunc(oh*a/2)*2:480" -c:v libx265 -crf 28 -codec:a libmp3lame -q:a 7 "${OUTPUT}"
     }
+
+    function ffmpeg-to-mp4-container {
+        INPUT=${1?Specify input file}
+        OUTPUT="${2:-${INPUT%.*}.mp4}"
+
+        ffmpeg -i "${INPUT}" -c:v copy -c:a aac "${OUTPUT}"
+    }
 fi
