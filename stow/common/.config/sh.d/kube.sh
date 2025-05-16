618f4e731c42272d0b0ecd5b61042fda9c308a10
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-05-16 20:52:47 +0100

    style: shellcheck comments

--- stow/common/.config/sh.d/kube.sh
+++ stow/common/.config/sh.d/kube.sh
@@ -4,14 +4,17 @@ if (command -v kubectl >/dev/null 2>&1); then
     alias kubectl-run-debian-bash='kubectl run --rm -i --tty --image=debian debian'
 
     function kubectl-context-select {
+        # shellcheck disable=SC2312
         kubectl config use-context "$(kubectl config get-contexts -o name | fzf)"
     }
 
     function kubectl-context-delete {
+        # shellcheck disable=SC2312
         kubectl config get-contexts -o name | fzf -m | xargs -n1 kubectl config delete-context
     }
 
     function kubectl-namespace-select {
+        # shellcheck disable=SC2312
         kubectl config set-context --current --namespace "$(kubectl get ns -o name | fzf -d/ --with-nth=2 | cut -d/ -f2)"
     }
 fi
