da48a716f8855d09ef60ccc6ffe05ceb18ecb88f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-05-17 22:50:07 +0100

    refactor: Detect filetype more cleanly

--- stow/if-command/nvim/.config/nvim/plugin/detect-filetype-when-empty.lua
+++ stow/if-command/nvim/.config/nvim/plugin/detect-filetype-when-empty.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "*",
     callback = function()
         if vim.bo.filetype == "" then
-            vim.cmd("filetype detect")
+            vim.cmd.filetype("detect")
         end
     end,
 })
