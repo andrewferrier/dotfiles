3311e9cbe6d2e7b2edd3d966f03bbdcfb1de11d0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-06-06 19:13:10 +0100

    fix: Temporarily disable _extui if in diff mode

--- stow/if-command/nvim/.config/nvim/plugin/cmdline-extui.lua
+++ stow/if-command/nvim/.config/nvim/plugin/cmdline-extui.lua
@@ -1,4 +1,6 @@
-if vim.fn.has("nvim-0.12.0") == 1 then
+-- FIXME: vim.opt.diff is temporary because of https://github.com/neovim/neovim/issues/34348
+
+if vim.fn.has("nvim-0.12.0") == 1 and not vim.opt.diff:get() then
     vim.o.cmdheight = 0
 
     require("vim._extui").enable({
