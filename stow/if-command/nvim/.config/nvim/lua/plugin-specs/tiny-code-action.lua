d6b92d8c84d1fc237f991a5c6d846ee92284bd96
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-07-12 18:50:05 +0100

    fix: Use fzf-lua with tiny-code-action

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/tiny-code-action.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/tiny-code-action.lua
@@ -4,9 +4,12 @@ return {
     "rachartier/tiny-code-action.nvim",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
+        { "ibhagwan/fzf-lua" },
     },
     event = "LspAttach",
-    opts = {},
+    opts = {
+        picker = "select",
+    },
     keys = {
         {
             "gra",
