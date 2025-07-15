7d843efb2b2734cc672bab52916f3a6ee3203b1e
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-07-16 00:36:58 +0100

    fix: Strip oil:// from filename if needed

--- stow/if-command/nvim/.config/nvim/plugin/file-list-fasd.lua
+++ stow/if-command/nvim/.config/nvim/plugin/file-list-fasd.lua
@@ -1,7 +1,19 @@
 vim.api.nvim_create_autocmd({ "BufReadPre", "BufWritePost" }, {
     callback = function(event)
+        local file = event.file
+
+        if file:sub(1, 6) == "oil://" then
+            file = file:sub(7)
+        end
+
         vim.system(
-            { "fasd", "-A", event.file },
+            { "fasd", "-A", file },
+            { detach = true },
+            require("utils").system_on_exit
+        )
+
+        vim.system(
+            { "/home/ferriera/memy/target/debug/memy", "--note", file },
             { detach = true },
             require("utils").system_on_exit
         )
--- stow/if-command/nvim/.config/nvim/plugin/file-list-memy.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufWritePost" }, {
-    callback = function(event)
-        vim.system(
-            { "/home/ferriera/memy/target/debug/memy", "--note", event.file },
-            { detach = true },
-            require("utils").system_on_exit
-        )
-    end,
-})
