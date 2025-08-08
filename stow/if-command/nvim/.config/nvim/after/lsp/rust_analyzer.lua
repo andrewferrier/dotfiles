6b507739406a2ceb6b62f08e6925fdca293e2ca4
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-08-08 23:07:43 +0100

    feat: Remove clippy args - come from Cargo.toml

--- stow/if-command/nvim/.config/nvim/after/lsp/rust_analyzer.lua
+++ stow/if-command/nvim/.config/nvim/after/lsp/rust_analyzer.lua
@@ -3,15 +3,6 @@ return {
         ["rust-analyzer"] = {
             check = {
                 command = "clippy",
-                extraArgs = {
-                    "--",
-                    "-D",
-                    "clippy::pedantic",
-                    "-D",
-                    "clippy::nursery",
-                    "-A",
-                    "clippy::cast_precision_loss",
-                },
             },
         },
     },
