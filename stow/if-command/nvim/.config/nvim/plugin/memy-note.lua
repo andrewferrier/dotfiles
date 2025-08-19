76cb74ee4b65fe111c101d5e8dc8457488e65bcc
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-08-19 22:14:05 +0100

    refactor: Dynamically link memy-note.lua

--- stow/if-command/nvim/.config/nvim/plugin/memy-note.lua
+++ stow/if-command/nvim/.config/nvim/plugin/memy-note.lua
@@ -1,15 +1,1 @@
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufWritePost" }, {
-    callback = function(event)
-        local file = event.file
-
-        if file:sub(1, 6) == "oil://" then
-            file = file:sub(7)
-        end
-
-        vim.system(
-            { "memy", "note", file },
-            { detach = true },
-            require("utils").system_on_exit
-        )
-    end,
-})
+../../../../../../../memy/hooks/neovim.lua
