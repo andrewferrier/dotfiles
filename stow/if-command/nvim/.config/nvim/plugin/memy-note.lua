5dc5b5d61557e94f514fc54e6383dcfa2ba0f1ae
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-08-31 14:56:44 +0100

    fix: Dynamically use memy hook only if available

--- stow/if-command/nvim/.config/nvim/plugin/memy-note.lua
+++ stow/if-command/nvim/.config/nvim/plugin/memy-note.lua
@@ -1,1 +1,10 @@
-../../../../../../../memy/hooks/neovim.lua
+local home = vim.fn.expand("~")
+local memy_dir = home .. "/memy"
+local hooks_file = memy_dir .. "/hooks/neovim.lua"
+
+if vim.fn.isdirectory(memy_dir) == 1 then
+    dofile(hooks_file)
+else
+    local output = vim.fn.system("memy hook neovim.lua")
+    vim.fn.luaeval(output)
+end
