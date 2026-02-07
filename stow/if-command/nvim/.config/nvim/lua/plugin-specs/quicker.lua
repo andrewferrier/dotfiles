77ece19d000c32b5c1125a4a3a12264e30f6835a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-02-07 17:26:09 +0000

    refactor: Add cleaner quicker capability

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/quicker.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/quicker.lua
@@ -5,5 +5,18 @@ return {
     ft = "qf",
     ---@module "quicker"
     ---@type quicker.SetupOptions
-    opts = {},
+    opts = {
+        keys = {
+            {
+                ">",
+                "<cmd>lua require('quicker').expand()<CR>",
+                desc = "Expand quickfix content",
+            },
+            {
+                "<",
+                "<cmd>lua require('quicker').collapse()<CR>",
+                desc = "Collapse quickfix content",
+            },
+        },
+    },
 }
--- stow/if-command/nvim/.config/nvim/plugin/quickfix.lua
+++ stow/if-command/nvim/.config/nvim/plugin/quickfix.lua
@@ -1,24 +1,11 @@
-local open = function()
-    if vim.tbl_count(vim.fn.getqflist()) == 0 then
-        vim.notify(
-            "Nothing in quickfix list; not opening.",
-            vim.log.levels.WARN
-        )
-    else
-        vim.cmd.copen()
-    end
-end
-
 vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
-    callback = open,
+    callback = function()
+        require("quicker").open()
+    end,
 })
 
 vim.keymap.set("n", "yoq", function()
-    if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then
-        vim.cmd.cclose()
-    else
-        open()
-    end
+    require("quicker").toggle()
 end, { desc = "Toggle quickfix window", unique = true })
 
 vim.cmd.packadd("cfilter")
