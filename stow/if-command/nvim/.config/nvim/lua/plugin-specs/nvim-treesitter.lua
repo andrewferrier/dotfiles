7fe4a9bcded7d43c55b37a803ae96a3300a646a4
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-03-30 17:03:06 +0100

    fix: Remove NeoVim < 0.12 support for treesitter

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
@@ -1,6 +1,13 @@
-local config_object = {
-    "nvim-treesitter/nvim-treesitter",
+-- Enable treesitter on all filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+    end,
+})
 
+-- selene: allow(mixed_table)
+return {
+    "nvim-treesitter/nvim-treesitter",
     build = function()
         vim.cmd.TSInstall("all")
         vim.cmd.TSUpdate()
@@ -9,32 +16,3 @@ local config_object = {
     -- function
     lazy = false,
 }
-
-if vim.fn.has("nvim-0.12.0") == 1 then
-    -- Enable treesitter on all filetypes
-    vim.api.nvim_create_autocmd("FileType", {
-        callback = function(args)
-            pcall(vim.treesitter.start, args.buf)
-        end,
-    })
-else
-    config_object.branch = "master"
-    config_object.config = function()
-        require("nvim-treesitter.configs").setup({
-            sync_install = true,
-            highlight = { enable = true },
-            refactor = {
-                smart_rename = {
-                    enable = true,
-                    keymaps = {
-                        smart_rename = false,
-                    },
-                },
-            },
-            indent = { enable = true },
-            endwise = { enable = true },
-        })
-    end
-end
-
-return config_object
