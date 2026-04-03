b3006b128189afd5add6c8d56813fddc44aab1e7
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-03 23:06:15 +0100

    refactor: Make git-prune-local clearer

--- stow/common/.config/sh.d/git.sh
+++ stow/common/.config/sh.d/git.sh
@@ -5,6 +5,12 @@ function git-commit-submodule() {
     git commit -m "Update commit on ${base}" "$1"
 }
 
-# shellcheck disable=SC2142
-alias git-prune-local="git fetch --prune; git branch -r | awk '{print \$1}' | grep -E -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print \$1}' | xargs git branch -d;"
+function git-prune-local() {
+    git fetch --prune
+    git branch -r \
+        | awk '{print $1}' \
+        | grep -E -v -f /dev/fd/0 <(git branch -vv | grep origin) \
+        | awk '{print $1}' \
+        | xargs git branch -d
+}
 alias gpc='while true; do git add -i -p; if ! git commit; then break; fi; done'
