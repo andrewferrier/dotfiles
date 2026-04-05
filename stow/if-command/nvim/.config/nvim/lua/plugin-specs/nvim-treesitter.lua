661bb896c3bfff48b3f27cfabe342ee0717a108a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-05 22:47:16 +0100

    refactor: start treesitter more reliably

--- stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/lua/plugin-specs/nvim-treesitter.lua
@@ -1,10 +1,3 @@
--- Enable treesitter on all filetypes
-vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
-    end,
-})
-
 -- selene: allow(mixed_table)
 return {
     "nvim-treesitter/nvim-treesitter",
@@ -13,6 +6,26 @@ return {
     build = function()
         vim.cmd.TSInstall("all")
         vim.cmd.TSUpdate()
+    end,
+    config = function()
+        require("nvim-treesitter").setup()
+
+        -- Enable treesitter on all installed filetypes
+        local parsers_installed =
+            require("nvim-treesitter").get_installed("parsers")
+        local treesitter_start_group =
+            vim.api.nvim_create_augroup("treesitter-start-files", {})
+
+        for _, parser in pairs(parsers_installed) do
+            local filetypes = vim.treesitter.language.get_filetypes(parser)
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                group = treesitter_start_group,
+                pattern = filetypes,
+                callback = function()
+                    vim.treesitter.start()
+                end,
+            })
+        end
     end,
     -- Don't use lazy initialization as TSInstall command is needed for build()
     -- function
