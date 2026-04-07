726358c623b8d3cd2cda687d0a9b08bb45b644aa
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-07 17:02:40 +0100

    refactor: Simplify LSP configuration

--- stow/if-command/nvim/.config/nvim/plugin/lsp.lua
+++ stow/if-command/nvim/.config/nvim/plugin/lsp.lua
@@ -1,19 +1,15 @@
----@param bufnr integer
-local function keybindings_inlayhints(bufnr)
-    vim.keymap.set("n", "yoi", function()
-        vim.lsp.inlay_hint.enable(
-            not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
-            { bufnr = bufnr }
-        )
-    end, { buffer = bufnr, desc = "Toggle inlay hints" })
-end
+vim.lsp.on_type_formatting.enable()
+vim.lsp.inlay_hint.enable()
 
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(event)
         local bufnr = event.buf
 
-        vim.lsp.on_type_formatting.enable()
-        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-        keybindings_inlayhints(bufnr)
+        vim.keymap.set("n", "yoi", function()
+            vim.lsp.inlay_hint.enable(
+                not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
+                { bufnr = bufnr }
+            )
+        end, { buffer = bufnr, desc = "Toggle inlay hints" })
     end,
 })
