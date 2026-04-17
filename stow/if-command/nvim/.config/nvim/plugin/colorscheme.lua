86de539b31358904a0644e60880a6eba68b7421b
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-17 18:25:11 +0100

    refactor: gruvbox -> vim.pack

---
+++ stow/if-command/nvim/.config/nvim/plugin/colorscheme.lua
@@ -0,0 +1,20 @@
+vim.pack.add({ { src = "https://github.com/ellisonleao/gruvbox.nvim" } })
+
+require("gruvbox").setup({
+    contrast = "hard",
+    dim_inactive = true,
+    transparent_mode = false,
+    overrides = {
+        DebugPrintLine = { link = "ErrorMsg" },
+        DiagnosticUnnecessary = { link = "Whitespace" },
+        OilInfo = { link = "NonText" },
+        SignColumn = { link = "LineNr" },
+        ["@lsp.mod.readonly"] = { bold = true },
+    },
+    ---@diagnostic disable-next-line missing-fields
+    italic = {
+        strings = false,
+    },
+})
+
+vim.cmd.colorscheme("gruvbox")
