b4d89bc6cc1670f6d3f8704e85799447b50afd10
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-20 14:01:25 +0100

    refactor: Simplify redraw_timer

--- stow/if-command/nvim/.config/nvim/plugin/statusline.lua
+++ stow/if-command/nvim/.config/nvim/plugin/statusline.lua
@@ -1,17 +1,16 @@
 local redraw_timer
 
-local delayed_redraw = function()
-    vim.schedule(vim.cmd.redrawstatus)
-    redraw_timer = nil
-end
-
 local redraw_status = function()
     if redraw_timer ~= nil then
         vim.fn.timer_stop(redraw_timer)
+        redraw_timer = nil
     end
 
     vim.schedule(vim.cmd.redrawstatus)
-    redraw_timer = vim.fn.timer_start(750, delayed_redraw)
+    redraw_timer = vim.fn.timer_start(750, function()
+        vim.schedule(vim.cmd.redrawstatus)
+        redraw_timer = nil
+    end)
 end
 
 vim.api.nvim_create_autocmd({ "DiagnosticChanged", "LspProgress" }, {
