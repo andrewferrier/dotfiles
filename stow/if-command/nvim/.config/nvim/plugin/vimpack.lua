225976166fda22ca6462165096e68f66b0dbcad0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-21 18:05:49 +0100

    feat: Generate helptags

--- stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
+++ stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
@@ -1,7 +1,13 @@
 local M = {}
 
-vim.api.nvim_create_user_command("VimPackUpdate", function(_)
+vim.api.nvim_create_user_command("PackUpdate", function(_)
     vim.pack.update(nil)
 end, {})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function()
+        vim.cmd.helptags("ALL")
+    end,
+})
 
 return M
