0cc8b433f0437ecfa46e9f1e696ad5ecd4b420e0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-26 12:30:34 +0100

    feat: Add PackClean command

--- stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
+++ stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
@@ -4,6 +4,19 @@ vim.api.nvim_create_user_command("PackUpdate", function(_)
     vim.pack.update(nil)
 end, {})
 
+vim.api.nvim_create_user_command("PackClean", function(_)
+    local inactive_names = vim.tbl_map(
+        function(plugin)
+            return plugin.spec.name
+        end,
+        vim.tbl_filter(function(plugin)
+            return not plugin.active
+        end, vim.pack.get())
+    )
+
+    vim.pack.del(inactive_names)
+end, {})
+
 vim.api.nvim_create_autocmd("PackChanged", {
     callback = function()
         vim.cmd.helptags("ALL")
