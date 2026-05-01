38f9595fec74cd74e8564c4e94f30886269602e1
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-01 09:32:38 +0100

    chore: Remove redundant lnav alias

--- stow/common/.config/sh.d/lnav.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if (command -v lnav >/dev/null 2>&1); then
-    alias lnav-24h='journalctl -f -o json --output-fields=MESSAGE,PRIORITY,_PID,SYSLOG_IDENTIFIER,_SYSTEMD_UNIT --since "24h ago" | lnav'
-fi
