e6477bc18752c9e4b6514aa5c1c1b317a32834b0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-05 23:22:47 +0100

    chore: Doesn't require plenary any more

--- stow/if-command/nvim/.config/nvim/plugin/diagnostics-lsp-linting/tiny-code-action.lua
+++ stow/if-command/nvim/.config/nvim/plugin/diagnostics-lsp-linting/tiny-code-action.lua
@@ -1,10 +1,4 @@
 vim.pack.add({ "https://github.com/rachartier/tiny-code-action.nvim" })
-vim.pack.add({
-    {
-        src = "https://github.com/nvim-lua/plenary.nvim",
-        version = vim.version.range("*"),
-    },
-})
 vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" })
 
 require("tiny-code-action").setup({ picker = "select" })
