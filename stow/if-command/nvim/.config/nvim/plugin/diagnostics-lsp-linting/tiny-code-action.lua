bf9f9da54af258db034b689a71e89d662b38d406
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-05 23:28:10 +0100

    fix: Let picker default so we get proper diffs

--- stow/if-command/nvim/.config/nvim/plugin/diagnostics-lsp-linting/tiny-code-action.lua
+++ stow/if-command/nvim/.config/nvim/plugin/diagnostics-lsp-linting/tiny-code-action.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/rachartier/tiny-code-action.nvim" })
 vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" })
 
-require("tiny-code-action").setup({ picker = "select" })
+require("tiny-code-action").setup({})
 
 vim.keymap.set("n", "gra", function()
     require("tiny-code-action").code_action({})
