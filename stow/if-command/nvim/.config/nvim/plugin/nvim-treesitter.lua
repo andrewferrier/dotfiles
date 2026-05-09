5235470cb386a7f7b154ba79b34904d60f833ecb
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-09 15:51:46 +0100

    refactor: Install treesitter parsers only when missing

--- stow/if-command/nvim/.config/nvim/plugin/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/plugin/nvim-treesitter.lua
@@ -4,10 +4,7 @@ vim.api.nvim_create_autocmd("PackChanged", {
             -- FIXME: Not sure why packadd is needed
             vim.cmd.packadd("nvim-treesitter")
 
-            if ev.data.kind == "install" then
-                vim.cmd.TSInstall("all")
-            elseif ev.data.kind == "update" then
-                vim.cmd.TSInstall("all")
+            if ev.data.kind == "update" then
                 vim.cmd.TSUpdate()
             end
         end
@@ -24,15 +21,31 @@ vim.pack.add({
 
 require("nvim-treesitter").setup()
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    group = vim.api.nvim_create_augroup("treesitter-start-files", {}),
-    pattern = vim.iter(require("nvim-treesitter").get_installed("parsers"))
-        :map(function(parser)
-            return vim.treesitter.language.get_filetypes(parser)
-        end)
-        :flatten()
-        :totable(),
-    callback = function()
-        vim.treesitter.start()
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+    group = vim.api.nvim_create_augroup("treesitter-start", { clear = true }),
+    callback = function(args)
+        local treesitter = require("nvim-treesitter")
+        local filetype = vim.bo[args.buf].filetype
+
+        if vim.bo[args.buf].buftype ~= "" or filetype == "" then
+            return
+        end
+
+        if
+            vim.list_contains(
+                treesitter.get_installed(),
+                vim.treesitter.language.get_lang(filetype)
+            )
+        then
+            vim.treesitter.start(args.buf)
+        else
+            vim.notify(
+                "Treesitter parser missing for "
+                    .. filetype
+                    .. "; installing..."
+            )
+            treesitter.install(filetype):wait(60000)
+            vim.treesitter.start(args.buf)
+        end
     end,
 })
