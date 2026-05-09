daed92cd8486ef6e121542cf3bbe3732b6fa76e3
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-09 16:38:06 +0100

    fix: Register parsers correctly

--- stow/if-command/nvim/.config/nvim/plugin/nvim-treesitter.lua
+++ stow/if-command/nvim/.config/nvim/plugin/nvim-treesitter.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_create_autocmd("PackChanged", {
     callback = function(ev)
         if ev.data.spec.name == "nvim-treesitter" then
-            -- FIXME: Not sure why packadd is needed
             vim.cmd.packadd("nvim-treesitter")
 
             if ev.data.kind == "update" then
@@ -11,15 +10,19 @@ vim.api.nvim_create_autocmd("PackChanged", {
     end,
 })
 
--- Without this, it keeps switching back to 'master' as the branch
+-- Without the version, it keeps switching back to 'master' as the branch
 vim.pack.add({
     {
         src = "https://github.com/nvim-treesitter/nvim-treesitter",
         version = "main",
     },
 })
+-- By doing packadd, it forces plugin/filetypes.lua to be loaded, which is
+-- important for filetype <-> lang mapping
+vim.cmd.packadd("nvim-treesitter")
 
 require("nvim-treesitter").setup()
+local available_to_install = require("nvim-treesitter").get_available()
 
 vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
     group = vim.api.nvim_create_augroup("treesitter-start", { clear = true }),
@@ -31,21 +34,20 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
             return
         end
 
-        if
-            vim.list_contains(
-                treesitter.get_installed(),
-                vim.treesitter.language.get_lang(filetype)
-            )
-        then
+        local treesitter_lang = vim.treesitter.language.get_lang(filetype)
+
+        if vim.list_contains(treesitter.get_installed(), treesitter_lang) then
             vim.treesitter.start(args.buf)
         else
-            vim.notify(
-                "Treesitter parser missing for "
-                    .. filetype
-                    .. "; installing..."
-            )
-            treesitter.install(filetype):wait(60000)
-            vim.treesitter.start(args.buf)
+            if vim.list_contains(available_to_install, treesitter_lang) then
+                vim.notify(
+                    "Treesitter parser missing for "
+                        .. filetype
+                        .. "; installing..."
+                )
+                treesitter.install(filetype):wait(60000)
+                vim.treesitter.start(args.buf)
+            end
         end
     end,
 })
