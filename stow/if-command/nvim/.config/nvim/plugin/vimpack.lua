2fa8992ff8d8a6f796cb811f68479f6d28efa982
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-05-17 10:02:06 +0100

    fix: Remove PackUpdateAndClean for NeoVim 0.13+
    Built-in now

--- stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
+++ stow/if-command/nvim/.config/nvim/plugin/vimpack.lua
@@ -1,19 +1,21 @@
 local M = {}
 
-vim.api.nvim_create_user_command("PackUpdateAndClean", function(_)
-    vim.pack.update(nil)
+if vim.fn.has("nvim-0.13") == 0 then
+    vim.api.nvim_create_user_command("PackUpdateAndClean", function(_)
+        vim.pack.update(nil)
 
-    local inactive_names = vim.tbl_map(
-        function(plugin)
-            return plugin.spec.name
-        end,
-        vim.tbl_filter(function(plugin)
-            return not plugin.active
-        end, vim.pack.get())
-    )
+        local inactive_names = vim.tbl_map(
+            function(plugin)
+                return plugin.spec.name
+            end,
+            vim.tbl_filter(function(plugin)
+                return not plugin.active
+            end, vim.pack.get())
+        )
 
-    vim.pack.del(inactive_names)
-end, {})
+        vim.pack.del(inactive_names)
+    end, {})
+end
 
 vim.api.nvim_create_autocmd("PackChanged", {
     callback = function()
