d7cb465ecc5b554108af0787db60ef64d588d8ae
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-06-21 21:41:13 +0100

    chore: update-background not needed; this is built in now

--- stow/if-command/nvim/.config/nvim/plugin/display/update-background.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({ "https://github.com/afonsofrancof/OSC11.nvim" })
-require("osc11").setup({
-    on_dark = function()
-        vim.opt.background = "dark"
-    end,
-    on_light = function()
-        vim.opt.background = "light"
-    end,
-})
