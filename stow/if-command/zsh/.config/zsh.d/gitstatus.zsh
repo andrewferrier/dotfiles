611f58affd577edd4917185edd45d67a4c6d8a8f
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-07-10 22:25:20 +0100

    chore: Format files

--- stow/if-command/zsh/.config/zsh.d/gitstatus.zsh
+++ stow/if-command/zsh/.config/zsh.d/gitstatus.zsh
@@ -3,63 +3,63 @@
 source $(antidote home)/**/*gitstatus*/gitstatus.plugin.zsh
 
 function gitstatus_prompt_update() {
-  emulate -L zsh
-  typeset -g  GITSTATUS_PROMPT=''
-  typeset -gi GITSTATUS_PROMPT_LEN=0
+    emulate -L zsh
+    typeset -g GITSTATUS_PROMPT=''
+    typeset -gi GITSTATUS_PROMPT_LEN=0
 
-  # Call gitstatus_query synchronously. Note that gitstatus_query can also be called
-  # asynchronously; see documentation in gitstatus.plugin.zsh.
-  gitstatus_query 'MY'                  || return 1  # error
-  [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
+    # Call gitstatus_query synchronously. Note that gitstatus_query can also be called
+    # asynchronously; see documentation in gitstatus.plugin.zsh.
+    gitstatus_query 'MY' || return 1                  # error
+    [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0 # not a git repo
 
-  local      clean='%2F'  # green
-  local   modified='%3F'  # yellow
-  local  untracked='%4F'  # blue
-  local conflicted='%1F'  # red
+    local clean='%2F'      # green
+    local modified='%3F'   # yellow
+    local untracked='%4F'  # blue
+    local conflicted='%1F' # red
 
-  local p
+    local p
 
-  local where  # branch name, tag or commit
-  if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-    where=$VCS_STATUS_LOCAL_BRANCH
-  elif [[ -n $VCS_STATUS_TAG ]]; then
-    p+='%f#'
-    where=$VCS_STATUS_TAG
-  else
-    p+='%f@'
-    where=${VCS_STATUS_COMMIT[1,8]}
-  fi
+    local where # branch name, tag or commit
+    if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
+        where=$VCS_STATUS_LOCAL_BRANCH
+    elif [[ -n $VCS_STATUS_TAG ]]; then
+        p+='%f#'
+        where=$VCS_STATUS_TAG
+    else
+        p+='%f@'
+        where=${VCS_STATUS_COMMIT[1,8]}
+    fi
 
-  (( $#where > 32 )) && where[13,-13]="…"  # truncate long branch names and tags
-  p+="${where//\%/%%}"             # escape %
+    (($#where > 32)) && where[13,-13]="…" # truncate long branch names and tags
+    p+="${where//\%/%%}"                  # escape %
 
-  # ⇣42 if behind the remote.
-  (( VCS_STATUS_COMMITS_BEHIND )) && p+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
-  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && p+=" "
-  (( VCS_STATUS_COMMITS_AHEAD  )) && p+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
-  # ⇠42 if behind the push remote.
-  (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && p+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
-  (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && p+=" "
-  # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
-  (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && p+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-  # *42 if have stashes.
-  (( VCS_STATUS_STASHES        )) && p+=" ${clean}*${VCS_STATUS_STASHES}"
-  # 'merge' if the repo is in an unusual state.
-  [[ -n $VCS_STATUS_ACTION     ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
-  # ~42 if have merge conflicts.
-  (( VCS_STATUS_NUM_CONFLICTED )) && p+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
-  # +42 if have staged changes.
-  (( VCS_STATUS_NUM_STAGED     )) && p+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
-  # !42 if have unstaged changes.
-  (( VCS_STATUS_NUM_UNSTAGED   )) && p+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
-  # ?42 if have untracked files. It's really a question mark, your font isn't broken.
-  (( VCS_STATUS_NUM_UNTRACKED  )) && p+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
+    # ⇣42 if behind the remote.
+    ((VCS_STATUS_COMMITS_BEHIND)) && p+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+    # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
+    ((VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND)) && p+=" "
+    ((VCS_STATUS_COMMITS_AHEAD)) && p+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+    # ⇠42 if behind the push remote.
+    ((VCS_STATUS_PUSH_COMMITS_BEHIND)) && p+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
+    ((VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND)) && p+=" "
+    # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
+    ((VCS_STATUS_PUSH_COMMITS_AHEAD)) && p+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
+    # *42 if have stashes.
+    ((VCS_STATUS_STASHES)) && p+=" ${clean}*${VCS_STATUS_STASHES}"
+    # 'merge' if the repo is in an unusual state.
+    [[ -n $VCS_STATUS_ACTION ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
+    # ~42 if have merge conflicts.
+    ((VCS_STATUS_NUM_CONFLICTED)) && p+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
+    # +42 if have staged changes.
+    ((VCS_STATUS_NUM_STAGED)) && p+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
+    # !42 if have unstaged changes.
+    ((VCS_STATUS_NUM_UNSTAGED)) && p+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+    # ?42 if have untracked files. It's really a question mark, your font isn't broken.
+    ((VCS_STATUS_NUM_UNTRACKED)) && p+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
 
-  GITSTATUS_PROMPT="${p}%f"
+    GITSTATUS_PROMPT="${p}%f"
 
-  # The length of GITSTATUS_PROMPT after removing %f and %F.
-  GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)}}"
+    # The length of GITSTATUS_PROMPT after removing %f and %F.
+    GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)/}}"
 }
 
 # Start gitstatusd instance with name "MY". The same name is passed to
--- stow/if-command/zsh/.config/zsh.d/zbell.zsh
+++ stow/if-command/zsh/.config/zsh.d/zbell.zsh
@@ -23,10 +23,10 @@ zmodload zsh/datetime || return
 autoload -Uz add-zsh-hook || return
 
 # initialize zbell_duration if not set
-(( ${+zbell_duration} )) || zbell_duration=15
+((${+zbell_duration})) || zbell_duration=15
 
 # initialize zbell_ignore if not set
-(( ${+zbell_ignore} )) || zbell_ignore=($EDITOR $PAGER)
+((${+zbell_ignore})) || zbell_ignore=($EDITOR $PAGER)
 
 # initialize it because otherwise we compare a date and an empty string
 # the first time we see the prompt. it's fine to have lastcmd empty on the
@@ -36,32 +36,32 @@ zbell_timestamp=$EPOCHSECONDS
 
 # right before we begin to execute something, store the time it started at
 zbell_begin() {
-	zbell_timestamp=$EPOCHSECONDS
-	zbell_lastcmd=$1
+    zbell_timestamp=$EPOCHSECONDS
+    zbell_lastcmd=$1
 }
 
 # when it finishes, if it's been running longer than $zbell_duration,
 # and we dont have an ignored command in the line, then print a bell.
 zbell_end() {
-	ran_long=$(( $EPOCHSECONDS - $zbell_timestamp >= $zbell_duration ))
+    ran_long=$(($EPOCHSECONDS - $zbell_timestamp >= $zbell_duration))
 
-	has_ignored_cmd=0
-	for cmd in ${(s:;:)zbell_lastcmd//|/;}; do
-		words=(${(z)cmd})
-		util=${words[1]}
-		if (( ${zbell_ignore[(i)$util]} <= ${#zbell_ignore} )); then
-			has_ignored_cmd=1
-			break
-		fi
-	done
+    has_ignored_cmd=0
+    for cmd in ${(s:;:)zbell_lastcmd//|/;}; do
+        words=(${(z)cmd})
+        util=${words[1]}
+        if ((${zbell_ignore[(i)$util]} <= ${#zbell_ignore})); then
+            has_ignored_cmd=1
+            break
+        fi
+    done
 
-	if (( ! $has_ignored_cmd )) && (( ran_long )); then
-		if command -v xdotool &>/dev/null; then
-			if [ "$(xdotool getwindowfocus)" -ne "$WINDOWID" ]; then
-				print -n "\a"
-			fi
-		fi
-	fi
+    if ((!$has_ignored_cmd)) && ((ran_long)); then
+        if command -v xdotool &>/dev/null; then
+            if [ "$(xdotool getwindowfocus)" -ne "$WINDOWID" ]; then
+                print -n "\a"
+            fi
+        fi
+    fi
 }
 
 # register the functions as hooks
